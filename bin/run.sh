4b99fec6ae4e1c3381dcc32e798f7c89c5d19607
Peter 'Pita' Martischka <petermartischka@googlemail.com>
2011-04-07 14:51:46 +0100

    Improve run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,15 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-#if [[ $EUID -eq 0 ]]; then
-#   echo "You shouldn't start LinePad as root!" 1>&2
-#   exit 1
-#fi
+if [[ $EUID -eq 0 ]]; then
+   echo "You shouldn't start Etherpad-Lite as root!" 1>&2
+   echo "Use authbind if you want to use a port lower than 1024 -> http://en.wikipedia.org/wiki/Authbind" 1>&2
+   exit 1
+fi
 
-#if [ ! type -P node &>/dev/null ]; then
-#  echo "You have no node installed!" 1>&2
-#  exit 1
-#fi
-#|| { echo "I require foo but it's not installed.  Aborting." >&2; exit 1; }
+type -P node &>/dev/null || { 
+  echo "You need to install node to run Etherpad-Lite!" >&2
+  exit 1 
+}
 
 cd ../node
-authbind node server.js
+node server.js
