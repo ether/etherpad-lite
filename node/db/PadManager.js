0e05e58c768b62e7f050562d8f17234c35dbd791
Peter 'Pita' Martischka <petermartischka@googlemail.com>
2011-08-11 15:26:41 +0100

    changed copyright from Peter 'Pita' Martischka to Peter 'Pita' Martischka (Primary Technology Ltd)

--- node/db/PadManager.js
+++ node/db/PadManager.js
@@ -3,7 +3,7 @@
  */
 
 /*
- * 2011 Peter 'Pita' Martischka
+ * 2011 Peter 'Pita' Martischka (Primary Technology Ltd)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
