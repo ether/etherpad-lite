b58bdb4e4d4f9b97502bd5207f37929223c7e948
Peter 'Pita' Martischka <petermartischka@googlemail.com>
2011-08-17 18:24:14 +0100

    support jsonp api calls

--- node/server.js
+++ node/server.js
@@ -290,6 +290,11 @@ async.waterfall([
       {
         response = JSON.stringify(response);
         apiLogger.info("RESPONSE, " + req.params.func + ", " + response);
+        
+        //is this a jsonp call, if yes, add the function call
+        if(req.query.jsonp)
+          response = req.query.jsonp + "(" + response + ")";
+        
         res._send(response);
       }
       
