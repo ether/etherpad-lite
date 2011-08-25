ded98250979cfb03d8f51b9ea8253b0a4d75eba8
Hans Pinckaers <hans.pinckaers@gmail.com>
2011-08-25 15:57:08 +0200

    Removed try/catch/finally and idleWorkTimer
    BUG: sometimes cursor gets hidden, can't type anymore.

--- bin/installDeps.sh
+++ bin/installDeps.sh
@@ -24,7 +24,7 @@ hash node > /dev/null 2>&1 || {
 NODE_VERSION=$(node --version)
 if [ ! $(echo $NODE_VERSION | cut -d "." -f 1-2) = "v0.4" ]; then
   echo "You're running a wrong version of node, you're using $NODE_VERSION, we need v0.4.x" >&2
-  exit 1 
+  # exit 1 
 fi
 
 #Is npm installed?
