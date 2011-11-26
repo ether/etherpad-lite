932ad0eeeb96d2eb75b29aa4e27fe89378089059
Peter 'Pita' Martischka <petermartischka@googlemail.com>
2011-11-25 16:58:00 -0800

    This may fixes the pdf convert issue

--- node/handler/ImportHandler.js
+++ node/handler/ImportHandler.js
@@ -81,7 +81,7 @@ exports.doImport = function(req, res, padId)
     //this allows us to accept source code files like .c or .java
     function(callback)
     {
-      var fileEnding = srcFile.split(".")[1];
+      var fileEnding = srcFile.split(".")[1].toLowerCase();
       var knownFileEndings = ["txt", "doc", "docx", "pdf", "odt", "html", "htm"];
       
       //find out if this is a known file ending
