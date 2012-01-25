87b11045e18ea27d90087021145addac2bdc8a6e
booo <borgers@mi.fu-berlin.de>
2012-01-25 22:00:08 +0100

    fix issue #281?

--- node/utils/Minify.js
+++ node/utils/Minify.js
@@ -28,7 +28,7 @@ var jsp = require("uglify-js").parser;
 var pro = require("uglify-js").uglify;
 var path = require('path');
 var Buffer = require('buffer').Buffer;
-var gzip = require('gzip');
+var zlib = require('zlib');
 var RequireKernel = require('require-kernel');
 var server = require('../server');
 var os = require('os');
@@ -233,23 +233,14 @@ function _handle(req, res, jsFilename, jsFiles) {
           //write the results compressed in a file
           function(callback)
           {
-            //spawn a gzip process if we're on a unix system
-            if(os.type().indexOf("Windows") == -1)
-            {
-              gzip(result, 9, function(err, compressedResult){
-                //weird gzip bug that returns 0 instead of null if everything is ok
-                err = err === 0 ? null : err;
+            zlib.gzip(result, function(err, compressedResult){
+              //weird gzip bug that returns 0 instead of null if everything is ok
+              err = err === 0 ? null : err;
+            
+              if(ERR(err, callback)) return;
               
-                if(ERR(err, callback)) return;
-                
-                fs.writeFile(CACHE_DIR + "minified_" + jsFilename + ".gz", compressedResult, callback);
-              });
-            }
-            //skip this step on windows
-            else
-            {
-              callback();
-            }
+              fs.writeFile(CACHE_DIR + "minified_" + jsFilename + ".gz", compressedResult, callback);
+            });
           }
         ],callback);
       }
