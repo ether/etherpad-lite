3f3bb950730e0ea8d959320677a230f79a546be5
booo <borgers@mi.fu-berlin.de>
2012-02-02 11:26:36 +0100

    Revert "Optimize javascript minification, slight increase of file size, but massive performance improvement"
    This reverts commit b32f883a9da5216300113a1f81fbd67fb793e7f1.

--- node/utils/Minify.js
+++ node/utils/Minify.js
@@ -326,6 +326,7 @@ function compressJS(values)
   var complete = values.join("\n");
   var ast = jsp.parse(complete); // parse code and get the initial AST
   ast = pro.ast_mangle(ast); // get a new AST with mangled names
+  ast = pro.ast_squeeze(ast); // get an AST with compression optimizations
   return pro.gen_code(ast); // compressed code here
 }
 
