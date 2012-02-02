c16d6eefdf2334e8bcb7621b5240635d8dc043a2
John McLear <john@mclear.co.uk>
2012-02-02 18:44:43 +0000

    remove callbacks to speed things up

--- static/js/chat.js
+++ static/js/chat.js
@@ -37,33 +37,8 @@ var chat = (function()
   var self = {
     show: function () 
     {      
-      $("#chaticon").hide(function ()
-      {
-        $("#chatbox").show();
-/*      $("#chatbox").resizable(
-        {
-          handles: 'nw',
-          minHeight: 40,
-          minWidth: 80,
-          start: function (event, ui)
-          {
-            $("#focusprotector").show();
-          },
-          stop: function (event, ui)
-          {
-            $("#focusprotector").hide();
-            
-            if(browser.mobile) {
-              $("#chatbox").css({right: "0px", bottom: "32px", left: "", top: ""});
-            } else {
-              $("#chatbox").css({right: "20px", bottom: "0px", left: "", top: ""});
-            }
-
-            self.scrollDown();
-          }
-        });
-*/
-      });
+      $("#chaticon").hide();
+      $("#chatbox").show();
       chatMentions = 0;
       document.title = title;
     },
