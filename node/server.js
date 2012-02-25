a5245c896aa6f89a855f7b3603fbb1e48fb06f64
Egil Moeller <egil.moller@freecode.no>
2012-02-25 15:20:31 +0100

    Moved apicalls

--- node/server.js
+++ node/server.js
@@ -161,67 +161,6 @@ async.waterfall([
       });
     });
     
-    var apiLogger = log4js.getLogger("API");
-
-    //This is for making an api call, collecting all post information and passing it to the apiHandler
-    var apiCaller = function(req, res, fields)
-    {
-      res.header("Content-Type", "application/json; charset=utf-8");
-    
-      apiLogger.info("REQUEST, " + req.params.func + ", " + JSON.stringify(fields));
-      
-      //wrap the send function so we can log the response
-      res._send = res.send;
-      res.send = function(response)
-      {
-        response = JSON.stringify(response);
-        apiLogger.info("RESPONSE, " + req.params.func + ", " + response);
-        
-        //is this a jsonp call, if yes, add the function call
-        if(req.query.jsonp)
-          response = req.query.jsonp + "(" + response + ")";
-        
-        res._send(response);
-      }
-      
-      //call the api handler
-      apiHandler.handle(req.params.func, fields, req, res);
-    }
-    
-    //This is a api GET call, collect all post informations and pass it to the apiHandler
-    app.get('/api/1/:func', function(req, res)
-    {
-      apiCaller(req, res, req.query)
-    });
-
-    //This is a api POST call, collect all post informations and pass it to the apiHandler
-    app.post('/api/1/:func', function(req, res)
-    {
-      new formidable.IncomingForm().parse(req, function(err, fields, files) 
-      {
-        apiCaller(req, res, fields)
-      });
-    });
-    
-    //The Etherpad client side sends information about how a disconnect happen
-    app.post('/ep/pad/connection-diagnostic-info', function(req, res)
-    {
-      new formidable.IncomingForm().parse(req, function(err, fields, files) 
-      { 
-        console.log("DIAGNOSTIC-INFO: " + fields.diagnosticInfo);
-        res.end("OK");
-      });
-    });
-    
-    //The Etherpad client side sends information about client side javscript errors
-    app.post('/jserror', function(req, res)
-    {
-      new formidable.IncomingForm().parse(req, function(err, fields, files) 
-      { 
-        console.error("CLIENT SIDE JAVASCRIPT ERROR: " + fields.errorInfo);
-        res.end("OK");
-      });
-    });
 
     //let the server listen
     app.listen(settings.port, settings.ip);
