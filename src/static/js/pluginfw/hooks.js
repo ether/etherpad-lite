349bb595b90fb9de9f7c73dca7a3fd30b969a4dc
Matthias Bartelmeß <mba@fourplusone.de>
2012-03-23 20:19:41 +0100

    corrected underscore path in hooks

--- src/static/js/pluginfw/hooks.js
+++ src/static/js/pluginfw/hooks.js
@@ -4,7 +4,7 @@ var _;
 /* FIXME: Ugly hack, in the future, use same code for server & client */
 if (plugins.isClient) {
   var async = require("ep_etherpad-lite/static/js/pluginfw/async");  
-  _ = require("underscore");
+  _ = require("ep_etherpad-lite/static/js/underscore");
 } else {
   var async = require("async");
   _ = require("underscore");
