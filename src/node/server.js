7f50e8f55106ca455e2897639454c31578bdb164
Matthias Bartelmeß <mba@fourplusone.de>
2012-04-20 22:40:33 +0200

    server.js can be called from console.

--- src/node/server.js
+++ src/node/server.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 /**
  * This module is started with bin/run.sh. It sets up a Express HTTP and a Socket.IO Server. 
  * Static file Requests are answered directly from this module, Socket.IO messages are passed 
