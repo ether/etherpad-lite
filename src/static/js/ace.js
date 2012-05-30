ether/etherpad-lite

commit 96ab3dc3b46a0f5d02d8ddb64d7ba2dcdb6b8aa6

jQuery is packaged - this is pure duplication.

src/static/js/ace.js
@@ -238,7 +238,6 @@ require.setGlobalKeyPath("require");\n\
 
       iframeHTML.push(doctype);
       iframeHTML.push("<html><head>");
-      iframeHTML.push('<script type="text/javascript" src="../static/js/jquery.js"></script>');
 
       hooks.callAll("aceInitInnerdocbodyHead", {
         iframeHTML: iframeHTML
@@ -279,6 +278,7 @@ require.setGlobalKeyPath("require");\n\
 ');
 
       iframeHTML.push('<script type="text/javascript">');
+      iframeHTML.push('$ = jQuery = require("ep_etherpad-lite/static/js/rjquery").jQuery; // Expose jQuery #HACK');
       iframeHTML.push('require("ep_etherpad-lite/static/js/ace2_inner");');
       iframeHTML.push('<\/script>');
 

src/templates/pad.html
@@ -313,7 +313,6 @@
 
         <% e.begin_block("scripts"); %>
         <script type="text/javascript" src="../static/js/require-kernel.js"></script>
-        <script type="text/javascript" src="../static/js/jquery.js"></script>
         <script type="text/javascript" src="../socket.io/socket.io.js"></script>
         <script type="text/javascript" src="../javascripts/lib/ep_etherpad-lite/static/js/pad.js?callback=require.define"></script>
         <script type="text/javascript" src="../javascripts/lib/ep_etherpad-lite/static/js/ace2_common.js?callback=require.define"></script>
@@ -330,6 +329,8 @@
               require.setRootURI(baseURL + "javascripts/src");
               require.setLibraryURI(baseURL + "javascripts/lib");
               require.setGlobalKeyPath("require");
+
+              $ = jQuery = require('ep_etherpad-lite/static/js/rjquery').jQuery; // Expose jQuery #HACK
 
               if ((!$.browser.msie) && (!($.browser.mozilla && $.browser.version.indexOf("1.8.") == 0))) {
                 document.domain = document.domain; // for comet

src/templates/timeslider.html
@@ -93,7 +93,6 @@
 </div>
 
 <script type="text/javascript" src="../../static/js/require-kernel.js"></script>
-<script type="text/javascript" src="../../static/js/jquery.js"></script>
 <script type="text/javascript" src="../../socket.io/socket.io.js"></script>
 <script type="text/javascript" src="../../javascripts/lib/ep_etherpad-lite/static/js/timeslider.js?callback=require.define"></script>
 <script type="text/javascript" src="../../javascripts/lib/ep_etherpad-lite/static/js/ace2_common.js?callback=require.define"></script>
@@ -113,6 +112,8 @@
     require.setRootURI(baseURL + "javascripts/src");
     require.setLibraryURI(baseURL + "javascripts/lib");
     require.setGlobalKeyPath("require");
+
+    $ = jQuery = require('ep_etherpad-lite/static/js/rjquery').jQuery; // Expose jQuery #HACK
 
     if ((!$.browser.msie) && (!($.browser.mozilla && $.browser.version.indexOf("1.8.") == 0))) {
       document.domain = document.domain; // for comet

commit 357ca73a998b4a76450321ae43cef2c40ffacb55

Bugfix for bug introduced by 96ab3dc3b46a0f5d02d8ddb64d7ba2dcdb6b8aa6: jQuery is packaged - this is pure duplication.

src/static/js/ace.js
@@ -239,10 +239,6 @@ require.setGlobalKeyPath("require");\n\
       iframeHTML.push(doctype);
       iframeHTML.push("<html><head>");
 
-      hooks.callAll("aceInitInnerdocbodyHead", {
-        iframeHTML: iframeHTML
-      });
-
       // calls to these functions ($$INCLUDE_...)  are replaced when this file is processed
       // and compressed, putting the compressed code from the named file directly into the
       // source here.
@@ -277,6 +273,11 @@ require.setGlobalKeyPath("require");\n\
       iframeHTML.push('<\/script>');
 
       iframeHTML.push('<style type="text/css" title="dynamicsyntax"></style>');
+
+      hooks.callAll("aceInitInnerdocbodyHead", {
+        iframeHTML: iframeHTML
+      });
+
       iframeHTML.push('</head><body id="innerdocbody" class="syntax" spellcheck="false">&nbsp;</body></html>');
 
       // Expose myself to global for my child frame.