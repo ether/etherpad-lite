40fe13907e84ba3c241ef3fe3877d0858ada9a98
Gedion <gwy321@gmail.com>
2012-07-04 20:42:36 -0500

    small fix in linestylefilter.js

--- src/static/js/linestylefilter.js
+++ src/static/js/linestylefilter.js
@@ -153,14 +153,20 @@ linestylefilter.getLineStyleFilter = function(lineLength, aline, textAndClassFun
           // prevent infinite loop if something funny's going on
           return nextAfterAuthorColors(txt, cls);
         }
-        var spanSize = txt.length;
-        if (spanSize > leftInAuthor)
-        {
-          spanSize = leftInAuthor;
+	var spanSize = txt.length;
+        if(txt&&txt.indexOf('data-tables')!=-1){		
+            spanSize = leftInAuthor = txt.length;        
         }
+        else if (spanSize > leftInAuthor){
+            spanSize = leftInAuthor;
+        }			
         var curTxt = txt.substring(0, spanSize);
-        txt = txt.substring(spanSize);
-        nextAfterAuthorColors(curTxt, (cls && cls + " ") + extraClasses);
+        txt = txt.substring(spanSize);				
+	if(curTxt&&curTxt.indexOf('data-tables')!=-1){		
+		nextAfterAuthorColors(curTxt,extraClasses );			
+	}else{
+		nextAfterAuthorColors(curTxt, (cls && cls + " ") + extraClasses);
+	}
         curIndex += spanSize;
         leftInAuthor -= spanSize;
         if (leftInAuthor == 0)
