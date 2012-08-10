d9d28369b2b456c6bb6208ce2ada8661c164af93
Marcel Klehr <mklehr@gmx.net>
2012-08-10 16:20:07 +0200

    Create author2sessions.sessionIDs if it doesn't exist yet.

--- src/node/db/SessionManager.js
+++ src/node/db/SessionManager.js
@@ -162,7 +162,7 @@ exports.createSession = function(groupID, authorID, validUntil, callback)
         if(ERR(err, callback)) return;
         
         //the entry doesn't exist so far, let's create it
-        if(author2sessions == null)
+        if(author2sessions == null || author2sessions.sessionIDs == null)
         {
           author2sessions = {sessionIDs : {}};
         }
