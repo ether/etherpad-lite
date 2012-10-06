7fd23c8d718eceeeb50469630547d4864a5f0d12
Peter 'Pita' Martischka <petermartischka@googlemail.com>
2012-10-06 21:34:20 +0200

    ported italic test to the new framework

--- tests/frontend/specs/button_italic.js
+++ tests/frontend/specs/button_italic.js
@@ -1,33 +1,33 @@
 describe("italic button", function(){
   //create a new pad before each test run
   beforeEach(function(cb){
-    testHelper.newPad(cb);
+    helper.newPad(cb);
   });
 
   it("makes text italic", function() {
-    //get the inner iframe
-    var $inner = testHelper.$getPadInner();
-    
+    var inner$ = helper.jQueryOf("inner"); 
+    var chrome$ = helper.jQueryOf("chrome"); 
+
     //get the first text element out of the inner iframe
-    var firstTextElement = $inner.find("div").first();
+    var $firstTextElement = inner$("div").first();
     
     //select this text element
-    testHelper.selectText(firstTextElement[0], $inner);
+    $firstTextElement.sendkeys('{selectall}');
 
     //get the bold button and click it
-    var $italicButton = testHelper.$getPadChrome().find(".buttonicon-italic");
-    $italicButton.click();
-
-    //ace creates a new dom element when you press a button, so just get the first text element again
-    var newFirstTextElement = $inner.find("div").first();
+    var $boldButton = chrome$(".buttonicon-italic");
+    $boldButton.click();
     
-    // is there a <b> element now?
-    var isItalic = newFirstTextElement.find("i").length === 1;
+    //ace creates a new dom element when you press a button, so just get the first text element again
+    var $newFirstTextElement = inner$("div").first();
+    
+    // is there a <i> element now?
+    var isItalic = $newFirstTextElement.find("i").length === 1;
 
     //expect it to be bold
     expect(isItalic).to.be(true);
 
     //make sure the text hasn't changed
-    expect(newFirstTextElement.text()).to.eql(firstTextElement.text());
+    expect($newFirstTextElement.text()).to.eql($firstTextElement.text());
   });
 });
