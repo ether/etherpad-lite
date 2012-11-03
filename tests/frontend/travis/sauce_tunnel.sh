c99a256acd520020e887e2a395b54620ed90ae32
Peter 'Pita' Martischka <petermartischka@googlemail.com>
2012-11-03 17:10:37 +0000

    Download sauce connect from google drive, thats much faster

--- tests/frontend/travis/sauce_tunnel.sh
+++ tests/frontend/travis/sauce_tunnel.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # download and unzip the sauce connector
-curl http://saucelabs.com/downloads/Sauce-Connect-latest.zip > /tmp/sauce.zip
-unzip /tmp/sauce.zip -d /tmp
+curl "https://doc-04-2c-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/2h0v0tdergb76jsikuo259nptvbvje4o/1351958400000/18059634261225994552/*/0Bx8MZz0WtyeGalRKeG9oRE1nRlk?e=download" | gunzip > /tmp/Sauce-Connect.jar
 
 # start the sauce connector in background and make sure it doesn't output the secret key
 (java -jar /tmp/Sauce-Connect.jar $SAUCE_USER $SAUCE_KEY -f /tmp/tunnel > /dev/null )&
