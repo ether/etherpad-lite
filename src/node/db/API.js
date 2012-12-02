6c3bcb33c0153a2ad35dcc10be6412847ee43c18
Josh Pruim <josh@josh-M11x-R2.(none)>
2012-12-01 22:29:48 -0800

    Added in an even better regex.

--- src/node/db/API.js
+++ src/node/db/API.js
@@ -331,7 +331,7 @@ exports.createPad = function(padID, text, callback)
     callback(new customError("createPad can't create group pads","apierror"));
     return;
   }
-  padID = padID.replace(/[;\/\?:@&=\+\$,{}\\\^\[\]\`\|]/gi, '_'); 
+  padID = padID.replace(/[;\/\?:@&=\+\$,{}\\\^\[\]\`\|%<>\*#]/gi, '_'); 
   //create pad
   getPadSafe(padID, false, text, function(err)
   {
