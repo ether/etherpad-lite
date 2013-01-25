7812d93e74dc1f76ae989454f9b746e3ac74ca15
mluto <m@luto.at>
2013-01-25 10:32:09 +0100

    Fixed "unnamed" and "enter your name" in userlist

--- src/static/js/pad_editor.js
+++ src/static/js/pad_editor.js
@@ -78,6 +78,12 @@ var padeditor = (function()
       
       html10n.bind('localized', function() {
         $("#languagemenu").val(html10n.getLanguage());
+        $('input[data-l10n-id]').each(function(key, input)
+          {
+            input = $(input);
+            if(input.hasClass("editempty"))
+              input.val(_(input.attr("data-l10n-id")));
+          });
       })
       $("#languagemenu").val(html10n.getLanguage());
       $("#languagemenu").change(function() {
