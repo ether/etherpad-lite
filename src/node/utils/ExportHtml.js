878fd7631c2f4df82620fa3b8642ba856519d584
Peter 'Pita' Martischka <petermartischka@googlemail.com>
2013-01-27 17:25:50 +0000

    Fixed HTML export

--- src/node/utils/ExportHtml.js
+++ src/node/utils/ExportHtml.js
@@ -91,8 +91,9 @@ function getPadHTML(pad, revNum, callback)
 }
 
 exports.getPadHTML = getPadHTML;
+exports.getHTMLFromAtext = getHTMLFromAtext;
 
-exports.getHTMLFromAtext = function(pad, atext, authorColors)
+function getHTMLFromAtext(pad, atext, authorColors)
 {
   var apool = pad.apool();
   var textLines = atext.text.slice(0, -1).split('\n');
