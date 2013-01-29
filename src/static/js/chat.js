daaa650a1be95e38e95f6fedff7e39a63cd847fe
John McLear <john@mclear.co.uk>
2013-01-29 12:15:11 +0000

    remove message about requiring comments

--- src/static/js/chat.js
+++ src/static/js/chat.js
@@ -1,10 +1,4 @@
 /**
- * This code is mostly from the old Etherpad. Please help us to comment this code. 
- * This helps other people to understand this code better and helps them to improve it.
- * TL;DR COMMENTS ON THIS FILE ARE HIGHLY APPRECIATED
- */
-
-/**
  * Copyright 2009 Google Inc., 2011 Peter 'Pita' Martischka (Primary Technology Ltd)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
