fa7952523e2ca14e8f97d4085cfa006cc22eb25a
John McLear <john@mclear.co.uk>
2013-01-29 02:08:37 +0000

    spacing

--- src/static/js/pad.js
+++ src/static/js/pad.js
@@ -365,10 +365,10 @@ function handshake()
 }
 
 $.extend($.gritter.options, { 
-        position: 'bottom-right', // defaults to 'top-right' but can be 'bottom-left', 'bottom-right', 'top-left', 'top-right' (added in 1.7.1)
-	fade_in_speed: 'medium', // how fast notifications fade in (string or int)
-	fade_out_speed: 2000, // how fast the notices fade out
-	time: 6000 // hang on the screen for...
+  position: 'bottom-right', // defaults to 'top-right' but can be 'bottom-left', 'bottom-right', 'top-left', 'top-right' (added in 1.7.1)
+  fade_in_speed: 'medium', // how fast notifications fade in (string or int)
+  fade_out_speed: 2000, // how fast the notices fade out
+  time: 6000 // hang on the screen for...
 });
 
 var pad = {
