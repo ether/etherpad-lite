594d27233449979ff0e4107e1e3a312cc1c3f9e7
John McLear <john@mclear.co.uk>
2013-02-03 00:14:17 +0000

    allow plugins to specify frontend test specs

--- src/node/hooks/express/tests.js
+++ src/node/hooks/express/tests.js
@@ -5,6 +5,17 @@ var path = require("path")
 exports.expressCreateServer = function (hook_name, args, cb) {
   args.app.get('/tests/frontend/specs_list.js', function(req, res){
     fs.readdir('tests/frontend/specs', function(err, files){
+
+      fs.readdir('node_modules', function(err, plugins){ // installed plugins
+        plugins.forEach(function(plugin){ // for each one
+          if(fs.existsSync("node_modules/"+plugin+"/tests/frontend/specs")){ // If the folder exists
+            fs.readdir("node_modules/"+plugin+"/tests/frontend/specs/", function(err, pluginFiles){
+              files.push(pluginFiles);
+            });
+          }
+        });
+      });
+
       if(err){ return res.send(500); }
 
       res.send("var specs_list = " + JSON.stringify(files.sort()) + ";\n"); 
