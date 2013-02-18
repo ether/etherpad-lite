eeeeb0484076c4b74614fc87526c649396e98000
John McLear <john@mclear.co.uk>
2013-02-18 00:37:47 +0000

    remove cruft

--- src/node/handler/PadMessageHandler.js
+++ src/node/handler/PadMessageHandler.js
@@ -926,7 +926,6 @@ function handleClientReady(client, message)
       })
 
       //If this is a reconnect, we don't have to send the client the ClientVars again
-      //if(message.reconnect == "not accepting true here makes everything work fine but it also sends whole atext which is bad")
       if(message.reconnect == true)
       {
         //Save the revision in sessioninfos, we take the revision from the info the client send to us
