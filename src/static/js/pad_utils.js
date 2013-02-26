b2eb1b381480d727f4ad7d2001ece661e4d497d1
John McLear <john@mclear.co.uk>
2013-02-26 13:14:17 +0000

    post url with pad error msg

--- src/static/js/pad_utils.js
+++ src/static/js/pad_utils.js
@@ -514,18 +514,18 @@ function setupGlobalExceptionHandler() {
   if (!globalExceptionHandler) {
     globalExceptionHandler = function test (msg, url, linenumber)
     {
+      var loc = document.location;
       var errorId = randomString(20);
       if ($("#editorloadingbox").attr("display") != "none"){
         //show javascript errors to the user
         $("#editorloadingbox").css("padding", "10px");
         $("#editorloadingbox").css("padding-top", "45px");
         $("#editorloadingbox").html("<div style='text-align:left;color:red;font-size:16px;'><b>An error occured</b><br>The error was reported with the following id: '" + errorId + "'<br><br><span style='color:black;font-weight:bold;font-size:16px'>Please send this error message to us: </span><div style='color:black;font-size:14px'>'"
-          + "ErrorId: " + errorId + "<br>UserAgent: " + navigator.userAgent + "<br>" + msg + " in " + url + " at line " + linenumber + "'</div></div>");
+          + "ErrorId: " + errorId + "<br>URL: " + loc + "<br>UserAgent: " + navigator.userAgent + "<br>" + msg + " in " + url + " at line " + linenumber + "'</div></div>");
       }
 
       //send javascript errors to the server
-      var errObj = {errorInfo: JSON.stringify({errorId: errorId, msg: msg, url: url, linenumber: linenumber, userAgent: navigator.userAgent})};
-      var loc = document.location;
+      var errObj = {errorInfo: JSON.stringify({errorId: errorId, url: loc, msg: msg, url: url, linenumber: linenumber, userAgent: navigator.userAgent})};
       var url = loc.protocol + "//" + loc.hostname + ":" + loc.port + "/" + loc.pathname.substr(1, loc.pathname.indexOf("/p/")) + "jserror";
  
       $.post(url, errObj);
