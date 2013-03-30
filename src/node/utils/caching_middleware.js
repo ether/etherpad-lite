ether/etherpad-lite

commit b3988e30d54ceccbf16a2586701e490fd037980c

pump isdeprecated

src/node/utils/caching_middleware.js
@@ -168,7 +168,7 @@ CachingMiddleware.prototype = new function () {
         } else if (req.method == 'GET') {
           var readStream = fs.createReadStream(pathStr);
           res.writeHead(statusCode, headers);
-          util.pump(readStream, res);
+          readableStream.pipe(readStream, res);
         } else {
           res.writeHead(statusCode, headers);
           res.end();

commit 358b07390e28338ddf6fa6e077fdbeb117431c99

fix "util.pump() is deprecated. Use readableStream.pipe()"
fix error introduced by b3988e3

src/node/utils/caching_middleware.js
@@ -168,7 +168,7 @@ CachingMiddleware.prototype = new function () {
         } else if (req.method == 'GET') {
           var readStream = fs.createReadStream(pathStr);
           res.writeHead(statusCode, headers);
-          readableStream.pipe(readStream, res);
+          readStream.pipe(res);
         } else {
           res.writeHead(statusCode, headers);
           res.end();