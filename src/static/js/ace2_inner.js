380821781f1fe5e8309ff58dde095d4add91ee1f
John McLear <john@mclear.co.uk>
2013-04-04 02:25:19 +0100

    dont use top, use the scheduler

--- src/static/js/ace2_inner.js
+++ src/static/js/ace2_inner.js
@@ -3651,7 +3651,7 @@ function Ace2Inner(){
           evt.preventDefault();
           var originalBackground = parent.parent.$('#revisionlink').css("background")
           parent.parent.$('#revisionlink').css({"background":"lightyellow"});
-          top.setTimeout(function(){
+          scheduler.setTimeout(function(){
             parent.parent.$('#revisionlink').css({"background":originalBackground});
           }, 1000);
           parent.parent.pad.collabClient.sendMessage({"type":"SAVE_REVISION"}); /* The parent.parent part of this is BAD and I feel bad..  It may break something */
