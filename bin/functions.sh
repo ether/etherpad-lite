8c6884f82c01e735692f2189525090ce4b15d098
Ilya Sher <ilya.sher@coding-knight.com>
2013-04-17 19:28:21 +0300

    node binary lookup: Fixed order (for real now) and organized a bit

--- bin/functions.sh
+++ bin/functions.sh
@@ -1,6 +1,6 @@
-NODEJS_BINARIES_CANDIDATES="node"
+NODEJS_BINARIES_CANDIDATES="nodejs node"
 
-NODEJS=nodejs
+NODEJS="NODE_JS_BINARY_NOT_FOUND"
 
 for candidate in $NODEJS_BINARIES_CANDIDATES; do
   if hash $candidate > /dev/null 2>&1; then
