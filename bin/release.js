c615ccf514e04e683885f7478df1bea95857f48c
John McLear <john@mclear.co.uk>
2013-09-27 14:47:34 +0100

    dont crash on importing blank html

---
+++ bin/release.js
@@ -0,0 +1,34 @@
+// A script to Release Etherpad.
+
+var etherRelease = {
+  init: function(){
+    // promptUser
+    promptUser(function(){
+      package.update(version){
+        
+
+      }
+    });
+  },
+  versionNumber: function(){
+    get: function(){
+      
+    }
+  },
+  
+
+  versionNumber.get: function(){
+
+
+  },
+
+  /* Continuous integration */
+  ci: function(){
+    hasPassed = function(){
+      // TODO
+      return true;
+    }
+  },
+
+
+}
