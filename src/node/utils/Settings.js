348d9a838f16450e6180822cf7c702ad1508523b
Robert Helmer <rhelmer@rhelmer.org>
2014-01-15 11:25:47 -0800

    convert over to server-side crypto

--- src/node/utils/Settings.js
+++ src/node/utils/Settings.js
@@ -26,7 +26,7 @@ var argv = require('./Cli').argv;
 var npm = require("npm/lib/npm.js");
 var jsonminify = require("jsonminify");
 var log4js = require("log4js");
-var randomString = require('ep_etherpad-lite/static/js/pad_utils').randomString;
+var randomString = require("./randomstring");
 
 
 /* Root path of the installation */
