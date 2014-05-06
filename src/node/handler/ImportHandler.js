6d38b056230feda140fd150170af17a861352fde
John McLear <john@mclear.co.uk>
2014-05-06 17:32:45 +0100

    abiword shouldnt be used for HTML native documents

--- src/node/handler/ImportHandler.js
+++ src/node/handler/ImportHandler.js
@@ -114,7 +114,10 @@ exports.doImport = function(req, res, padId)
     //convert file to html
     function(callback) {
       if(!importHandledByPlugin){
-        if (abiword) {
+        var fileEnding = path.extname(srcFile).toLowerCase();
+        var fileIsHTML = (fileEnding === ".html" || fileEnding === ".htm");
+        if (abiword && !fileIsHTML) {
+          console.warn("Using abiword");
           abiword.convertFile(srcFile, destFile, "htm", function(err) {
             //catch convert errors
             if(err) {
