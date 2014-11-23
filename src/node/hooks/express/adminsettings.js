ae7da122d7d35e1da38280f12defd65a899859a1
John McLear <john@mclear.co.uk>
2014-11-23 14:14:01 +0000

    fix session management

--- src/node/hooks/express/adminsettings.js
+++ src/node/hooks/express/adminsettings.js
@@ -23,8 +23,7 @@ exports.socketio = function (hook_name, args, cb) {
   var io = args.io.of("/settings");
   io.on('connection', function (socket) {
 
-    console.warn ("The middleware now handles auth but I'm not convinced SocketIO is being responsible enough here so this needs reviewing before hitting master");
-    // if (!socket.handshake.session || !socket.handshake.session.user || !socket.handshake.session.user.is_admin) return;
+    if (!socket.conn.request.session || !socket.conn.request.session.user || !socket.conn.request.session.user.is_admin) return;
 
     socket.on("load", function (query) {
       fs.readFile('settings.json', 'utf8', function (err,data) {
