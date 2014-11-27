f5ff25e3756b959d4ec095bf00b70b1593125b71
John McLear <john@mclear.co.uk>
2014-11-27 00:29:19 +0000

    utf8 check

--- src/node/db/DB.js
+++ src/node/db/DB.js
@@ -50,6 +50,15 @@ exports.init = function(callback)
     //everything ok
     else
     {
+      // Checks for UTF8
+      db.set("testUTF8", "मानकहिन्दी");
+      db.get("testUTF8", function(err,v){
+        if(v !== "मानकहिन्दी"){
+          console.error("Database is not storing UTF8, this could hurt you a lot");
+          process.exit(1);
+        }
+      })
+
       exports.db = db;  
       callback(null);
     }
