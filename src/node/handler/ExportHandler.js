5ba3cab445e4909275b97fc7414fa56c6c94578d
John McLear <john@mclear.co.uk>
2014-12-30 00:13:01 +0100

    better take some responsibility

--- src/node/handler/ExportHandler.js
+++ src/node/handler/ExportHandler.js
@@ -4,6 +4,7 @@
 
 /*
  * 2011 Peter 'Pita' Martischka (Primary Technology Ltd)
+ * 2014 John McLear (Etherpad Foundation / McLear Ltd)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
