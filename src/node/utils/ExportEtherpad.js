ac4f9eb4cebc0e8dbaa0df425aae660e3fc22071
John McLear <john@mclear.co.uk>
2014-12-30 00:12:26 +0100

    licensing

--- src/node/utils/ExportEtherpad.js
+++ src/node/utils/ExportEtherpad.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2014 John McLear.
+ * 2014 John McLear (Etherpad Foundation / McLear Ltd)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
