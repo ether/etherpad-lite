aca745ddf6a180e17c461bfb352281ee1f3acb1c
John McLear <john@mclear.co.uk>
2015-01-25 02:44:10 +0000

    tell installer if old etherpad needs updating during plugin install

--- src/static/js/admin/plugins.js
+++ src/static/js/admin/plugins.js
@@ -225,6 +225,9 @@ $(document).ready(function () {
 
   socket.on('finished:install', function(data) {
     if(data.error) {
+      if(data.code === "EPEERINVALID"){
+        alert("This plugin requires that you update Etherpad so it can operate in it's true glory");
+      }
       alert('An error occured while installing '+data.plugin+' \n'+data.error)
       $('#installed-plugins .'+data.plugin).remove()
     }
