ether/etherpad-lite

commit 3354b9406b94e1a04b5eee1c0152914dde73ba89

op is ok, if it doesnt include newlines. op is ok, if op.lines is equal to newlines in op.chars

src/static/js/Changeset.js
@@ -929,7 +929,7 @@ exports.applyToText = function (cs, str) {
       removedLines += op.lines;
       newlines = strIter.newlines()
       strIter.skip(op.chars);
-      if(!(newlines - strIter.newlines() == op.lines)){
+      if(!(newlines - strIter.newlines() == 0) && (newlines - strIter.newlines() != op.lines)){
         newlinefail = true
       }
       break;

commit c191a8716ebee37f61cdd4b07aff41f52e90c604

totally wrong, introduced by myself in 3354b9406b94e1a04b5eee1c0152914dde73ba89 the first part is always false so the second part which is always true in case a cs deleted some lines was never triggered...sigh

src/static/js/Changeset.js
@@ -927,16 +927,12 @@ exports.applyToText = function (cs, str) {
       break;
     case '-':
       removedLines += op.lines;
-      newlines = strIter.newlines()
       strIter.skip(op.chars);
-      if(!(newlines - strIter.newlines() == 0) && (newlines - strIter.newlines() != op.lines)){
-        newlinefail = true
-      }
       break;
     case '=':
       newlines = strIter.newlines()
       assem.append(strIter.take(op.chars));
-      if(!(newlines - strIter.newlines() == op.lines)){
+      if(newlines - strIter.newlines() != op.lines){
         newlinefail = true
       }
       break;