ef38bcad9ff6e929ad0aa87b260ca73154270774
John McLear <john@mclear.co.uk>
2015-03-31 13:57:18 +0100

    make buttons i18n friendly and a better experience

--- src/node/utils/toolbar.js
+++ src/node/utils/toolbar.js
@@ -99,7 +99,7 @@ _.extend(Button.prototype, {
     };
     return tag("li", liAttributes,
       tag("a", { "class": this.grouping, "data-l10n-id": this.attributes.localizationId },
-        tag("button", { "class": " "+ this.attributes.class, "title": this.attributes.command })
+        tag("button", { "class": " "+ this.attributes.class, "alt": this.attributes.command, "data-l10n-id": this.attributes.localizationId })
       )
     );
   }
