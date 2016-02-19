53fd05493c46b2c4316c00b4d4dd3058c6f29196
Benjamin Schweizer <cxcv@cxcv.de>
2016-02-19 10:07:01 +0100

    made url relative

--- src/static/js/pad.js
+++ src/static/js/pad.js
@@ -872,7 +872,7 @@ var pad = {
       $.ajax(
       {
         type: 'post',
-        url: '/ep/pad/connection-diagnostic-info',
+        url: 'ep/pad/connection-diagnostic-info',
         data: {
           diagnosticInfo: JSON.stringify(pad.diagnosticInfo)
         },
