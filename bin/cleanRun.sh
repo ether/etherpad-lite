5f9de6948c46f76f5e01a4040094bc77bcf3a345
muxator <a.mux@inwind.it>
2018-08-17 22:22:14 +0200

    cleanRun.sh: get rid of "Bad substitution" error
    It was introduced in 4a18f0d97d96 (#3356).

--- bin/cleanRun.sh
+++ bin/cleanRun.sh
@@ -38,4 +38,4 @@ bin/installDeps.sh $* || exit 1
 echo "Started Etherpad..."
 
 SCRIPTPATH=`pwd -P`
-node "${$SCRIPTPATH}/node_modules/ep_etherpad-lite/node/server.js" $*
+node "${SCRIPTPATH}/node_modules/ep_etherpad-lite/node/server.js" $*

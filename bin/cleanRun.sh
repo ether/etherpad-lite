ether/etherpad-lite

commit 4a18f0d97d9654a6a3749b67b0f24f0c7d5c94ff

Fix bug with cleanRun.sh
Now works if the output of `pwd` has a space in it.

bin/cleanRun.sh
@@ -38,4 +38,4 @@ bin/installDeps.sh $* || exit 1
 echo "Started Etherpad..."
 
 SCRIPTPATH=`pwd -P`
-node $SCRIPTPATH/node_modules/ep_etherpad-lite/node/server.js $*
+node "${$SCRIPTPATH}/node_modules/ep_etherpad-lite/node/server.js" $*

commit 5f9de6948c46f76f5e01a4040094bc77bcf3a345

cleanRun.sh: get rid of "Bad substitution" error
It was introduced in 4a18f0d97d96 (#3356).

bin/cleanRun.sh
@@ -38,4 +38,4 @@ bin/installDeps.sh $* || exit 1
 echo "Started Etherpad..."
 
 SCRIPTPATH=`pwd -P`
-node "${$SCRIPTPATH}/node_modules/ep_etherpad-lite/node/server.js" $*
+node "${SCRIPTPATH}/node_modules/ep_etherpad-lite/node/server.js" $*