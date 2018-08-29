61823e7689fcdec71a5119a6c246eada9f5ed450
muxator <a.mux@inwind.it>
2018-08-29 02:28:40 +0200

    db/AuthorManager: early return, no functional changes

--- src/node/db/AuthorManager.js
+++ src/node/db/AuthorManager.js
@@ -209,20 +209,19 @@ exports.listPadsOfAuthor = function (authorID, callback)
     if(author == null)
     {
       callback(new customError("authorID does not exist","apierror"))
+      return;
     }
+
     //everything is fine, return the pad IDs
-    else
+    var pads = [];
+    if(author.padIDs != null)
     {
-      var pads = [];
-      if(author.padIDs != null)
+      for (var padId in author.padIDs)
       {
-        for (var padId in author.padIDs)
-        {
-          pads.push(padId);
-        }
+        pads.push(padId);
       }
-      callback(null, {padIDs: pads});
     }
+    callback(null, {padIDs: pads});
   });
 }
 
