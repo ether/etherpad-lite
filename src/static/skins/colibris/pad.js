8881a2a61fd2215249b776f4b87f7f5316a65819
Sebastian Castro <90scastro@gmail.com>
2018-11-08 21:56:26 +0100

    colibris: style plugin ep_tables2

--- src/static/skins/colibris/pad.js
+++ src/static/skins/colibris/pad.js
@@ -1,6 +1,4 @@
 function customStart()
 {
-  //define your javascript here
-  //jquery is available - except index.js
-  //you can load extra scripts with $.getScript http://api.jquery.com/jQuery.getScript/
+  $('#pad_title').show();
 }
