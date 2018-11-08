385ca8771b8a1cf5bc9ff0260d08f9e9d16d3c3c
Sebastian Castro <90scastro@gmail.com>
2018-11-08 21:56:40 +0100

    colibris: various improvements, including author_hover, cursortrace plugins

--- src/static/skins/colibris/pad.js
+++ src/static/skins/colibris/pad.js
@@ -1,16 +1,4 @@
 function customStart()
 {
   $('#pad_title').show();
-
-  $('.hyperlink-icon').on('click',function() {
-    $('.hyperlink-dialog').appendTo('body').css({'top': $('.hyperlink-icon').offset().top + 42, 'left': $('.hyperlink-icon').offset().left - 12});
-  });
-  $('.hyperlink-url').on("keyup", function(e)
-  {
-    if(e.keyCode == 13) // touche entrée
-    { 
-      $('.hyperlink-save').click();
-    }
-  });
-  $('.hyperlink-save').click(function() { $('.hyperlink-dialog').hide(); });
 }
