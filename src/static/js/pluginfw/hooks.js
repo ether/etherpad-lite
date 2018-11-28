ether/etherpad-lite

commit 23eab79946e40f92c87371e9cad3ed56d93b3ed2

pad.html: for each client plugin, add a class to #editorcontainerbox
This commit implements the following behaviour:

1. adds a function clientPluginNames() to hooks.js (mimicking what is done in
   static.js), which returns an array containing the list of currently installed
   client side plugins. The array is eventually empty.

2. calls that function in pad.html at rendering time (thus server-side) to
   populate a class attribute.

Example results:
- with no client-side plugins installed:
  <div id="editorcontainerbox" class="">

- with some client-side plugins installed:
  <div id="editorcontainerbox" class="ep_author_neat ep_adminpads">

Looking at the existing code (src/node/hooks/express/static.js#L39-L57), a
client-side plugin is defined as a plugin that implements at least a client side
hook.

NOTE: there is currently no support for notifying plugin removal/installation
      to the connected clients: for now, in order to get an updated class list,
      the clients will have to refresh the page.

Fixes #3488

src/static/js/pluginfw/hooks.js
@@ -125,3 +125,29 @@ exports.callAllStr = function(hook_name, args, sep, pre, post) {
   }
   return newCallhooks.join(sep || "");
 }
+
+/*
+ * Returns an array containing the names of the installed client-side plugins
+ *
+ * If no client-side plugins are installed, returns an empty array.
+ * Duplicate names are always discarded.
+ *
+ * A client-side plugin is a plugin that implements at least one client_hook
+ *
+ * EXAMPLE:
+ *   No plugins:   []
+ *   Some plugins: [ 'ep_adminpads', 'ep_add_buttons', 'ep_activepads' ]
+ */
+exports.clientPluginNames = function() {
+  if (!(exports.plugins)) {
+    return [];
+  }
+
+  var client_plugin_names = [...new Set(
+    exports.plugins.parts
+      .filter(part => part.hasOwnProperty('client_hooks'))
+      .map(part => part['plugin'])
+  )];
+
+  return client_plugin_names;
+}

src/templates/pad.html
@@ -100,7 +100,7 @@
             <% e.end_block(); %>
         </div>
 
-        <div id="editorcontainerbox">
+        <div id="editorcontainerbox" class="<%=hooks.clientPluginNames().join(' '); %>">
             <div id="editorcontainer" class="editorcontainer"></div>
             <div id="editorloadingbox">
               <div id="passwordRequired">

commit 0ad8291ae778b80da49bc6ea263927c67e53d23d

hooks: restore Internet Explorer 11 compatibility.
Compatibility with IE11 regressed in 23eab79946e4 while working for #3488.
That commit made use of modern js syntax, not supported by IE11.

- Removed arrow functions, replaced with normal functions.
- Removed the spread operator (<...iterable>) and the "new Set()" construct,
  replaced with _.uniq()

At some point IE11 compatibility will be dropped.
Ditching it now, for such a small gain, is not wise.

Fixes #3500.

src/static/js/pluginfw/hooks.js
@@ -143,11 +143,11 @@ exports.clientPluginNames = function() {
     return [];
   }
 
-  var client_plugin_names = [...new Set(
+  var client_plugin_names = _.uniq(
     exports.plugins.parts
-      .filter(part => part.hasOwnProperty('client_hooks'))
-      .map(part => part['plugin'])
-  )];
+      .filter(function(part) { return part.hasOwnProperty('client_hooks'); })
+      .map(function(part) { return part['plugin']; })
+  );
 
   return client_plugin_names;
 }