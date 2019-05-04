ca978d47c4973550562dc50602470a64df1cd430
muxator <a.mux@inwind.it>
2019-05-04 04:53:55 +0200

    WIP: requireable dependencies, explicit entrypoint
    --HG--
    branch : browserify-2019

--- src/static/js/create-bundle.sh
+++ src/static/js/create-bundle.sh
@@ -3,6 +3,15 @@
 set -eu
 
 npx --no-install browserify \
+  -r ./ace2_inner.js:ep_etherpad-lite/static/js/ace2_inner \
+  -r ./browser.js:ep_etherpad-lite/static/js/browser \
+  -r ./chat.js:ep_etherpad-lite/static/js/chat \
+  -r ./pad_editbar.js:ep_etherpad-lite/static/js/pad_editbar \
+  -r ./pad_impexp.js:ep_etherpad-lite/static/js/pad_impexp \
+  -r ./pad.js:ep_etherpad-lite/static/js/pad \
+  -r ./pluginfw/client_plugins.js:ep_etherpad-lite/static/js/pluginfw/client_plugins \
+  -r ./pluginfw/hooks.js:ep_etherpad-lite/static/js/pluginfw/hooks \
+  -r ./rjquery.js:ep_etherpad-lite/static/js/rjquery \
   ace2_common.js \
   ace.js \
   AttributeManager.js \
@@ -10,11 +19,9 @@ npx --no-install browserify \
   broadcast.js \
   excanvas.js \
   pad_editor.js \
-  pad.js \
   security.js \
   skiplist.js \
   timeslider.js \
   underscore.js \
   undomodule.js \
-  --entry=entrypoint.js \
   --outfile=bundle.js
