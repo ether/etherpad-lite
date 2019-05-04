175b4024d1dc86bb6a26f93a49c775a108dc1c19
muxator <a.mux@inwind.it>
2019-05-04 02:13:11 +0200

    factor out entrypoint.js
    --HG--
    branch : browserify-2019

---
+++ src/static/js/entrypoint.js
@@ -0,0 +1,37 @@
+var clientVars = {};
+(function () {
+  var pathComponents = location.pathname.split('/');
+
+  // Strip 'p' and the padname from the pathname and set as baseURL
+  var baseURL = pathComponents.slice(0,pathComponents.length-2).join('/') + '/';
+
+  require.setRootURI(baseURL + "javascripts/src");
+  require.setLibraryURI(baseURL + "javascripts/lib");
+  require.setGlobalKeyPath("require");
+
+  $ = jQuery = require('ep_etherpad-lite/static/js/rjquery').jQuery; // Expose jQuery #HACK
+  browser = require('ep_etherpad-lite/static/js/browser');
+
+  var plugins = require('ep_etherpad-lite/static/js/pluginfw/client_plugins');
+  var hooks = require('ep_etherpad-lite/static/js/pluginfw/hooks');
+
+  plugins.baseURL = baseURL;
+  plugins.update(function () {
+    hooks.plugins = plugins;
+
+    // Call documentReady hook
+    $(function() {
+      hooks.aCallAll('documentReady');
+    });
+
+    var pad = require('ep_etherpad-lite/static/js/pad');
+    pad.baseURL = baseURL;
+    pad.init();
+  });
+
+  /* TODO: These globals shouldn't exist. */
+  pad = require('ep_etherpad-lite/static/js/pad').pad;
+  chat = require('ep_etherpad-lite/static/js/chat').chat;
+  padeditbar = require('ep_etherpad-lite/static/js/pad_editbar').padeditbar;
+  padimpexp = require('ep_etherpad-lite/static/js/pad_impexp').padimpexp;
+}());
