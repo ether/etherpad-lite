b82816c774ebdff076375fb5705b9f720ac9de67
muxator <a.mux@inwind.it>
2019-12-07 04:22:54 +0100

    express: reformat session configuration in preparation for the next commit
    No functional changes.

--- src/node/hooks/express/webaccess.js
+++ src/node/hooks/express/webaccess.js
@@ -123,7 +123,17 @@ exports.expressConfigure = function (hook_name, args, cb) {
   }
 
   args.app.sessionStore = exports.sessionStore;
-  args.app.use(sessionModule({secret: exports.secret, store: args.app.sessionStore, resave: true, saveUninitialized: true, name: 'express_sid', proxy: true, cookie: { secure: !!settings.ssl }}));
+  args.app.use(sessionModule({
+    secret: exports.secret,
+    store: args.app.sessionStore,
+    resave: true,
+    saveUninitialized: true,
+    name: 'express_sid',
+    proxy: true,
+    cookie: {
+      secure: !!settings.ssl,
+    }
+  }));
 
   args.app.use(cookieParser(settings.sessionKey, {}));
 
