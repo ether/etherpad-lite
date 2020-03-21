ca3548827387bddc492dd53717de9e8b3ca1efaf
muxator <a.mux@inwind.it>
2020-03-22 00:33:22 +0100

    minify: upgrade clean-css 3.4.19 -> 4.2.3. Adapt to the semver major change
    CleanCSS 3.4.19 had a Regex Denial of Service vulnerability and has to be
    updated. The major version bump requires the following changes:
    
    1. Disabling rebase is necessary because otherwise the URLs for the web fonts
       become wrong;
    
       EXAMPLE 1:
           /static/css/src/static/font/fontawesome-etherpad.woff
         instead of
           /static/font/fontawesome-etherpad.woff
    
       EXAMPLE 2 (this is more surprising):
           /p/src/static/font/opendyslexic.otf
         instead of
           /static/font/opendyslexic.otf
    
    2. CleanCSS.minify() can either receive a string containing the CSS, or an array
       of strings. In that case each array element is interpreted as an absolute
       local path from which the CSS file is read.
    
       In version 4.x, CleanCSS API was simplified, eliminating the relativeTo
       parameter, and thus we cannot use our already loaded "content" argument, but
       we have to wrap the absolute path to the CSS in an array and ask the library
       to read it by itself.
    
    Fixes #3616.

--- src/node/utils/Minify.js
+++ src/node/utils/Minify.js
@@ -414,11 +414,36 @@ function compressJS(content)
 function compressCSS(filename, content, callback)
 {
   try {
-    var base = path.join(ROOT_DIR, path.dirname(filename));
-    new CleanCSS({relativeTo: base}).minify(content, function (errors, minified) {
+    const absPath = path.join(ROOT_DIR, filename);
+
+    /*
+     * Changes done to migrate CleanCSS 3.x -> 4.x:
+     *
+     * 1. Disabling rebase is necessary because otherwise the URLs for the web
+     *    fonts become wrong.
+     *
+     *    EXAMPLE 1:
+     *        /static/css/src/static/font/fontawesome-etherpad.woff
+     *      instead of
+     *        /static/font/fontawesome-etherpad.woff
+     *    EXAMPLE 2 (this is more surprising):
+     *        /p/src/static/font/opendyslexic.otf
+     *      instead of
+     *        /static/font/opendyslexic.otf
+     *
+     * 2. CleanCSS.minify() can either receive a string containing the CSS, or
+     *    an array of strings. In that case each array element is interpreted as
+     *    an absolute local path from which the CSS file is read.
+     *
+     *    In version 4.x, CleanCSS API was simplified, eliminating the
+     *    relativeTo parameter, and thus we cannot use our already loaded
+     *    "content" argument, but we have to wrap the absolute path to the CSS
+     *    in an array and ask the library to read it by itself.
+     */
+    new CleanCSS({rebase: false}).minify([absPath], function (errors, minified) {
       if (errors) {
         // on error, just yield the un-minified original, but write a log message
-        console.error(`CleanCSS.minify() returned an error on ${filename} (base CSS path: ${base}): ${errors}`);
+        console.error(`CleanCSS.minify() returned an error on ${filename} (${absPath}): ${errors}`);
         callback(null, content);
       } else {
         callback(null, minified.styles);
@@ -426,7 +451,7 @@ function compressCSS(filename, content, callback)
     });
   } catch (error) {
     // on error, just yield the un-minified original, but write a log message
-    console.error(`Unexpected error minifying ${filename} (base CSS path: ${base}): ${error}`);
+    console.error(`Unexpected error minifying ${filename} (${absPath}): ${error}`);
     callback(null, content);
   }
 }
