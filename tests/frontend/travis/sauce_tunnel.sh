79427d15b76b606c3229945e13f6425da7c86456
John McLear <john@mclear.co.uk>
2020-03-22 11:16:53 +0000

    tests: explicit tunnel version URI in sauce_tunnel.sh
    For an explanation, see bf0bb58c703f.

--- tests/frontend/travis/sauce_tunnel.sh
+++ tests/frontend/travis/sauce_tunnel.sh
@@ -6,7 +6,7 @@
 #          example https://saucelabs.com/downloads/sc-4.5.4-linux.tar.gz
 #          Supported versions are currently listed at:
 #          https://wiki.saucelabs.com/display/DOCS/Downloading+Sauce+Connect+Proxy
-curl https://saucelabs.com/downloads/sc-latest-linux.tar.gz > /tmp/sauce.tar.gz
+curl https://saucelabs.com/downloads/sc-4.5.4-linux.tar.gz > /tmp/sauce.tar.gz
 tar zxf /tmp/sauce.tar.gz --directory /tmp
 mv /tmp/sc-*-linux /tmp/sauce_connect
 
