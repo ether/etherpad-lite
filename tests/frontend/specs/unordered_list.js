ff71b04f0e6050f3f0d4e19b09a6d598b2b82f87
John McLear <john@mclear.co.uk>
2020-03-27 13:37:37 +0000

    test coverage for #2812

--- tests/frontend/specs/unordered_list.js
+++ tests/frontend/specs/unordered_list.js
@@ -33,3 +33,40 @@ describe("assign unordered list", function(){
   });
 
 });
+
+describe("assign unordered list", function(){
+  //create a new pad before each test run
+  beforeEach(function(cb){
+    helper.newPad(cb);
+    this.timeout(60000);
+  });
+
+  it("insert unordered list text then remove by clicking list again", function(done){
+    var inner$ = helper.padInner$;
+    var chrome$ = helper.padChrome$;
+    var originalText = inner$("div").first().text();
+
+    var $insertunorderedlistButton = chrome$(".buttonicon-insertunorderedlist");
+    $insertunorderedlistButton.click();
+
+    helper.waitFor(function(){
+      var newText = inner$("div").first().text();
+      if(newText === originalText){
+        return inner$("div").first().find("ul li").length === 1;
+      }
+    }).done(function(){
+
+      // remove indentation by bullet and ensure text string remains the same
+      $insertunorderedlistButton.click();
+      helper.waitFor(function(){
+        var isList = inner$("div").find("ul").length === 1;
+        // sohuldn't be list
+        return (isList === false);
+      }).done(function(){
+        done();
+      });
+
+    });
+  });
+
+});
