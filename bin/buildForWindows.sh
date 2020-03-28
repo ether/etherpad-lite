a181ea8fbe9f81017f84d4d1bff7632a992a5847
muxator <a.mux@inwind.it>
2020-03-29 00:53:17 +0100

    bin: replace double backticks (``) with $()
    This has been the recommended way of launching subshells for ages, and is easier
    to type and on the eye.
    
    For a quick reference, see:
    https://unix.stackexchange.com/questions/5778/whats-the-difference-between-stuff-and-stuff#5782

--- bin/buildForWindows.sh
+++ bin/buildForWindows.sh
@@ -3,7 +3,7 @@
 NODE_VERSION="10.18.0"
 
 #Move to the folder where ep-lite is installed
-cd `dirname $0`
+cd $(dirname $0)
 
 #Was this script started in the bin folder? if yes move out
 if [ -d "../bin" ]; then
--- bin/installDeps.sh
+++ bin/installDeps.sh
@@ -49,7 +49,7 @@ require_minimal_version() {
 }
 
 #Move to the folder where ep-lite is installed
-cd `dirname $0`
+cd $(dirname $0)
 
 #Was this script started in the bin folder? if yes move out
 if [ -d "../bin" ]; then
