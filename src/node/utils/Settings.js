ether/etherpad-lite

commit 28a3bba4c10627ee17ddc3b09282818ad06a0db7

settings: fix incorrect implementation of conditional user creation.
The change that implemented #3648 (7c099fef5e08) was incorrect, and resulted
in disabling every user at startup.

The problem was twofold:
1. _.filter() on an object returns an array of the object's enumerable values
   and strips out the keys, see: https://stackoverflow.com/questions/11697702/how-to-use-underscore-js-filter-with-an-object
   To filter an object, the function that needs to be used is _.pick();

2. The logic condition on userProperties.password was plain wrong (it should
   have been an AND instead of an OR).

This change corrects 1) and 2), and writes more specific logs when something
goes wrong.

Closes #3661.

src/node/utils/Settings.js
@@ -637,14 +637,21 @@ exports.reloadSettings = function reloadSettings() {
      * This is used by the settings.json in the default Dockerfile to eschew
      * creating an admin user if no password is set.
      */
-    var filteredUsers = _.filter(exports.users, function(userProperties, username) {
-      if ((userProperties.hasOwnProperty("password")) || userProperties.password !== null) {
-        return true;
+    var filteredUsers = _.pick(exports.users, function(userProperties, username) {
+      if (userProperties.hasOwnProperty("password") === false) {
+        console.warn(`Removing user "${username}", because it has no "password" field.`);
+
+        return false;
       }
 
-      console.warn(`The password for ${username} is null. This means the user must not be created. Removing it.`);
+      if (userProperties.password === null) {
+        console.warn(`Removing user "${username}", because its password is null.`);
 
-      return false;
+        return false;
+      }
+
+      // This user has a password, and its password is not null. Keep it.
+      return true;
     });
 
     exports.users = filteredUsers;

commit a581bbd80dcb50842ac6241ea2907f0115dac28f

Settings: allow the existence of "password" and "hash" attributes for users.
This brings back compatibility with ep_hash_auth, which was inadvertently broken
with 28a3bba4c106.
Based on work by John McLear.

Fixes #3681.

src/node/utils/Settings.js
@@ -631,26 +631,50 @@ exports.reloadSettings = function reloadSettings() {
 
   if (exports.users) {
     /*
-     * Prune from export.users any user that has no password attribute, or whose
-     * password attribute is "null".
+     * Each user must have exactly one of ("password", "hash") attributes set,
+     * and its value must be not null.
      *
-     * This is used by the settings.json in the default Dockerfile to eschew
-     * creating an admin user if no password is set.
+     * Prune from export.users any user that does not satisfy this condition,
+     * including the ones that (by chance) have both "password" and "hash" set.
+     *
+     * This mechanism is used by the settings.json in the default Dockerfile to
+     * eschew creating an admin user if no password (or hash) is set.
      */
     var filteredUsers = _.pick(exports.users, function(userProperties, username) {
-      if (userProperties.hasOwnProperty("password") === false) {
-        console.warn(`Removing user "${username}", because it has no "password" field.`);
+      if ((userProperties.hasOwnProperty("password") === false) && (userProperties.hasOwnProperty("hash") === false)) {
+        console.warn(`Removing user "${username}", because it has no "password" or "hash" field.`);
 
         return false;
       }
 
-      if (userProperties.password === null) {
-        console.warn(`Removing user "${username}", because its password is null.`);
+      if (userProperties.hasOwnProperty("password") && userProperties.hasOwnProperty("hash")) {
+        console.warn(`Removing user "${username}", because it has both "password" and "hash" fields set. THIS SHOULD NEVER HAPPEN.`);
 
         return false;
       }
 
-      // This user has a password, and its password is not null. Keep it.
+      /*
+       * If we arrive here, the user has exactly a password or a hash set.
+       * They may still be null
+       */
+      if (userProperties.hasOwnProperty("password") && (userProperties.password === null)) {
+        console.warn(`Removing user "${username}", because its "password" is null.`);
+
+        return false;
+      }
+
+      if (userProperties.hasOwnProperty("hash") && (userProperties.hash === null)) {
+        console.warn(`Removing user "${username}", because its "hash" value is null.`);
+
+        return false;
+      }
+
+      /*
+       * This user has a password, and its password is not null, or it has an
+       * hash, and its hash is not null (not both).
+       *
+       * Keep it.
+       */
       return true;
     });
 