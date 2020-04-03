c79ad1e504df16bc24545aa5545b63576fe65c61
Sebastian Castro <sebastian.castro@protonmail.com>
2020-04-03 17:41:47 +0200

    css: fix Colibris timeslider

--- src/static/skins/colibris/timeslider.js
+++ src/static/skins/colibris/timeslider.js
@@ -1,6 +1,3 @@
 function customStart()
 {
-  console.log("custom start", $('#timeslider-wrapper').length);
-  // inverse display order betwwen slidebar and titles
-  $('#timeslider-wrapper').appendTo('#timeslider-top');
 }
