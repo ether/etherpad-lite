0e10ac828f1c9428200e1704b4fa2e21356731ba
John McLear <john@mclear.co.uk>
2020-04-07 17:20:40 +0000

    cleaning up more, noting that thing sare weird

--- tests/backend/specs/api/contentcollector.js
+++ tests/backend/specs/api/contentcollector.js
@@ -1,38 +1,38 @@
-var Changeset = require("../../../../src/static/js/Changeset");
-var contentcollector = require("../../../../src/static/js/contentcollector");
-var AttributePool = require("../../../../src/static/js/AttributePool");
-var cheerio = require("../../../../src/node_modules/cheerio");
-var util = require('util');
+const Changeset = require("../../../../src/static/js/Changeset");
+const contentcollector = require("../../../../src/static/js/contentcollector");
+const AttributePool = require("../../../../src/static/js/AttributePool");
+const cheerio = require("../../../../src/node_modules/cheerio");
+const util = require('util');
 
-var html = "<html><body><ol class='list-number1' start='1'><li>a</li></ol><ol class='list-number1' start='2'><li>b</li></ol><ol class='list-number1' start='3'><li>c</li></ol><ol class='list-number1' start='4'><li>defg</li></ol></body></html>";
-var $ = cheerio.load(html);
-var doc = $('html')[0];
+//    html : "<html><body><ol class='list-number1' start='1'><li>a</li></ol><ol class='list-number1' start='2'><li>b</li></ol><ol class='list-number1' start='3'><li>c</li></ol><ol class='list-number1' start='4'><li>defg</li></ol></body></html>",
 
-// Convert a dom tree into a list of lines and attribute liens
-// using the content collector object
-var apool = new AttributePool();
-var cc = contentcollector.makeContentCollector(true, null, apool);
-
-  try {
-    // we use a try here because if the HTML is bad it will blow up
-    cc.collectContent(doc);
-  } catch(e) {
-    // don't process the HTML because it was bad
-    throw e;
+const tests = {
+  ol: {
+    description : "Tests if ols properly get line numbers when in a normal OL",
+    html : "<html><body><ol><li>a</li><li>b</li><li>c</li><li>defg</li><li>whyonly3</li><li>always misses last</li>wtf</ol></body></html>",
+    expectedLineAttribs : {},
+    expectedText: ["*a","*b","*c", "*defg"]
   }
+}
+
+// For each test..
+for (var test in tests){
+  var $ = cheerio.load(tests[test].html);  // Load HTML into Cheerio
+  var doc = $('html')[0]; // Creates a dom-like representation of HTML
+
+  // Create an empty attribute pool
+  var apool = new AttributePool();
+  // Convert a dom tree into a list of lines and attribute liens
+  // using the content collector object
+  var cc = contentcollector.makeContentCollector(true, null, apool);
+  cc.collectContent(doc);
 
   var result = cc.finish();
-  // console.warn("result", result);
+  var recievedAttributes = result.lineAttribs;
+  var recievedText = result.lines
+  // expect recievedAttributes === expectedLineAttribs
+  // expect recievedText === expectedText TODO
 
-  //  result.lineAttribs =  [ '*0*1*1*3+1|1+2', '*0*1*6|1+1', '*0*1*1*3+1|1+2', '*0*1*6+1|1+2', '', '+4' ]
-  // console.warn("forcedresult", result);
+  console.warn(recievedAttributes, recievedText);
 
-  var i;
-  for (i = 0; i < result.lines.length; i++) {
-    console.log('Line ' + (i + 1) + ' attributes: ' + result.lineAttribs[i]);
-  }
-
-  // Get the new plain text and its attributes
-  var newText = result.lines.join('\n');
-  var newAttribs = result.lineAttribs.join('|1+1') + '|1+1';
-
+}
