e57421e96766e58ac92f941434e957e6196682d7
Alexandre Magno <alexanmtz@gmail.com>
2020-05-28 14:03:39 +0200

    adding js script that parses the html to send results

--- tests/frontend/travis/remote_runner.js
+++ tests/frontend/travis/remote_runner.js
@@ -54,8 +54,23 @@ var sauceTestWorker = async.queue(function (testSettings, callback) {
     var knownConsoleText = "";
     var getStatusInterval = setInterval(function(){
       browser.eval("$('#mocha-report').html()", function(err, consoleText){
-        console.log("console text", consoleText, err)
-        console.log("jquery test", $)
+        var wrapperReport = $('</div>').html(consoleText)
+        var globalResult = ''
+        var report = wrapperReport.clone()
+        var reportRaw = report.find('pre').remove()
+        reportRaw.find('> li').each(function(index, element) {
+          var titleText = $(element).find('> h1').text()
+          var testResultTitle = $(element).find('> ul > li > h1').text()
+          var result = '';
+          $(element).find('> ul > li > ul > li').each(function(index, element) {
+            if($(element).hasClass('pass')) result += '[green]PASSED[clear] : ';
+            if($(element).hasClass('fail')) result += '[red]FAILED[clear] : ';
+            if($(element).hasClass('pending')) result += '[yellow]PENDING[clear] : ';
+            result += $(element).text();
+          });   
+          globalResult += titleText + '\n' + testResultTitle + '\n -> ' + result + '\n\n'; 
+        });
+        console.log('global result'. globalResult)
         if(!consoleText || err){
           return;
         }
