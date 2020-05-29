7c45e38d5ae86955705edee2c6f0e47b1617c9a7
John McLear <john@mclear.co.uk>
2020-05-29 11:25:10 +0000

    hopefully final commit for tihs problem

--- tests/frontend/travis/remote_runner.js
+++ tests/frontend/travis/remote_runner.js
@@ -78,14 +78,12 @@ sauceTestWorker.push({
   , 'browserName'    : 'firefox'
   , 'version'        : 'latest'
 });
-/*
 // 2) Chrome on Linux
 sauceTestWorker.push({
     'platform'       : 'Linux'
   , 'browserName'    : 'googlechrome'
   , 'version'        : 'latest'
 });
-*/
 // 3) Safari on OSX 10.15
 sauceTestWorker.push({
     'platform'       : 'OS X 10.15'
