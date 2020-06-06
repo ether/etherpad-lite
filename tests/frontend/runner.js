2ce798339d42bf426e19dbad93ea729fcf41820e
John McLear <john@mclear.co.uk>
2020-06-06 18:57:52 +0000

    Revert "tests: updating mocha and refactoring the custom html reporter using events"
    This reverts commit df1629c7b9898e5d9bc787dd2c96e4176271cddc.

--- tests/frontend/runner.js
+++ tests/frontend/runner.js
@@ -1,26 +1,5 @@
 $(function(){
-
-  function stringifyException(exception){
-    var err = exception.stack || exception.toString();
-
-    // FF / Opera do not add the message
-    if (!~err.indexOf(exception.message)) {
-      err = exception.message + '\n' + err;
-    }
-
-    // <=IE7 stringifies to [Object Error]. Since it can be overloaded, we
-    // check for the result of the stringifying.
-    if ('[object Error]' == err) err = exception.message;
-
-    // Safari doesn't give you a stack. Let's at least provide a source line.
-    if (!exception.stack && exception.sourceURL && exception.line !== undefined) {
-      err += "\n(" + exception.sourceURL + ":" + exception.line + ")";
-    }
-
-    return err;
-  }
-
-  function CustomRunner(runner) {
+  function Base(runner) {
     var self = this
       , stats = this.stats = { suites: 0, tests: 0, passes: 0, pending: 0, failures: 0 }
       , failures = this.failures = [];
@@ -35,42 +14,11 @@ $(function(){
     runner.on('suite', function(suite){
       stats.suites = stats.suites || 0;
       suite.root || stats.suites++;
-      if (suite.root) return;
-      append(suite.title);
-      level++;
     });
 
-    runner.on('suite end', function(suite){
-      if (suite.root) return;
-      level--;
-
-      if(level == 0) {
-        append("");
-      }
-    });
-
-    // Scroll down test display after each test
-    mocha = $('#mocha')[0];
-    runner.on('test', function(){
-      mocha.scrollTop = mocha.scrollHeight;
-    });
-
-    var killTimeout;
     runner.on('test end', function(test){
       stats.tests = stats.tests || 0;
       stats.tests++;
-      if ('passed' == test.state) {
-        append("->","[green]PASSED[clear] :", test.title);
-      } else if (test.pending) {
-        append("->","[yellow]PENDING[clear]:", test.title);
-      } else {
-        append("->","[red]FAILED[clear] :", test.title, stringifyException(test.err));
-      }
-
-      if(killTimeout) clearTimeout(killTimeout);
-      killTimeout = setTimeout(function(){
-        append("FINISHED - [red]no test started since 3 minutes, tests stopped[clear]");
-      }, 60000 * 3);
     });
 
     runner.on('pass', function(test){
@@ -101,8 +49,26 @@ $(function(){
     runner.on('pending', function(){
       stats.pending++;
     });
+  }
 
-    var $console = $("#console");
+  /*
+    This reporter wraps the original html reporter plus reports plain text into a hidden div.
+    This allows the webdriver client to pick up the test results
+  */
+  var WebdriverAndHtmlReporter = function(html_reporter){
+    return function(runner){
+      Base.call(this, runner);
+
+      // Scroll down test display after each test
+      mocha = $('#mocha')[0];
+      runner.on('test', function(){
+        mocha.scrollTop = mocha.scrollHeight;
+      });
+
+      //initalize the html reporter first
+      html_reporter(runner);
+
+      var $console = $("#console");
       var level = 0;
       var append = function(){
         var text = Array.prototype.join.apply(arguments, [" "]);
@@ -131,6 +97,58 @@ $(function(){
         $console.text(oldText + newText + "\\n");
       }
 
+      runner.on('suite', function(suite){
+        if (suite.root) return;
+
+        append(suite.title);
+        level++;
+      });
+
+      runner.on('suite end', function(suite){
+        if (suite.root) return;
+        level--;
+
+        if(level == 0) {
+          append("");
+        }
+      });
+
+      var stringifyException = function(exception){
+        var err = exception.stack || exception.toString();
+
+        // FF / Opera do not add the message
+        if (!~err.indexOf(exception.message)) {
+          err = exception.message + '\n' + err;
+        }
+
+        // <=IE7 stringifies to [Object Error]. Since it can be overloaded, we
+        // check for the result of the stringifying.
+        if ('[object Error]' == err) err = exception.message;
+
+        // Safari doesn't give you a stack. Let's at least provide a source line.
+        if (!exception.stack && exception.sourceURL && exception.line !== undefined) {
+          err += "\n(" + exception.sourceURL + ":" + exception.line + ")";
+        }
+
+        return err;
+      }
+
+      var killTimeout;
+      runner.on('test end', function(test){
+        if ('passed' == test.state) {
+          append("->","[green]PASSED[clear] :", test.title);
+        } else if (test.pending) {
+          append("->","[yellow]PENDING[clear]:", test.title);
+        } else {
+          append("->","[red]FAILED[clear] :", test.title, stringifyException(test.err));
+        }
+
+        if(killTimeout) clearTimeout(killTimeout);
+        killTimeout = setTimeout(function(){
+          append("FINISHED - [red]no test started since 3 minutes, tests stopped[clear]");
+        }, 60000 * 3);
+      });
+
       var total = runner.total;
       runner.on('end', function(){
         if(stats.tests >= total){
@@ -140,6 +158,7 @@ $(function(){
           append("FINISHED -", stats.passes, "tests passed,", stats.failures, "tests failed, duration: " + minutes + ":" + seconds);
         }
       });
+    }
   }
 
   //http://stackoverflow.com/questions/1403888/get-url-parameter-with-jquery
@@ -170,7 +189,10 @@ $(function(){
       mocha.grep(grep);
     }
 
-    var runner = mocha.run();
-    CustomRunner(runner)
+    mocha.ignoreLeaks();
+
+    mocha.reporter(WebdriverAndHtmlReporter(mocha._reporter));
+
+    mocha.run();
   });
 });
