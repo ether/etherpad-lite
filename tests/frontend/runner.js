a974d45066bd136a4661e30ac8d49c87d7eef9ce
webzwo0i <webzwo0i@c3d2.de>
2020-07-10 21:38:12 +0200

    try to include test.speed in output

--- tests/frontend/runner.js
+++ tests/frontend/runner.js
@@ -60,7 +60,7 @@ $(function(){
       stats.tests = stats.tests || 0;
       stats.tests++;
       if ('passed' == test.state) {
-        append("->","[green]PASSED[clear] :", test.title);
+        append("->","[green]PASSED[clear] :", test.title," ",test.speed,"ms");
       } else if (test.pending) {
         append("->","[yellow]PENDING[clear]:", test.title);
       } else {
