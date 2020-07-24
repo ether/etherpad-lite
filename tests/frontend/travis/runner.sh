1442675346a8eeb8d6d893b791c4ce8948dc0c44
webzwo0i <webzwo0i@c3d2.de>
2020-07-24 15:06:48 +0200

    remote_runner will take some time to setup sl, so this second is not necessary

--- tests/frontend/travis/runner.sh
+++ tests/frontend/travis/runner.sh
@@ -30,9 +30,6 @@ echo "Now I will try for 15 seconds to connect to Etherpad on http://localhost:9
 
 echo "Successfully connected to Etherpad on http://localhost:9001"
 
-# just in case, let's wait for another second before going on
-sleep 1
-
 # On the Travis VM, remote_runner.js is found at
 # /home/travis/build/ether/[secure]/tests/frontend/travis/remote_runner.js
 # which is the same directory that contains this script.
