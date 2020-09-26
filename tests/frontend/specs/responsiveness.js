f80a8c9b10fb87d277ebdd7bc344d092d3a02d9f
John McLear <john@mclear.co.uk>
2020-09-26 19:40:12 +0100

    readd responiveness coverage

--- tests/frontend/specs/responsiveness.js
+++ tests/frontend/specs/responsiveness.js
@@ -24,7 +24,7 @@ describe('Responsiveness of Editor', function() {
   // And the test needs to be fixed to work in Firefox 52 on Windows 7.  I am not sure why it fails on this specific platform
   // The errors show this.timeout... then crash the browser but I am sure something is actually causing the stack trace and
   // I just need to narrow down what, offers to help accepted.
-  xit('Fast response to keypress in pad with large amount of contents', function(done) {
+  it('Fast response to keypress in pad with large amount of contents', function(done) {
 
     //skip on Windows Firefox 52.0
     if(window.bowser && window.bowser.windows && window.bowser.firefox && window.bowser.version == "52.0") {
