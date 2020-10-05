55f427b53d48e89ce61c8b658e8e056dae841f82
webzwo0i <webzwo0i@c3d2.de>
2020-10-05 23:32:16 +0200

    increase helper.waitFor time limits

--- tests/frontend/specs/timeslider_follow.js
+++ tests/frontend/specs/timeslider_follow.js
@@ -77,23 +77,23 @@ describe("timeslider", function(){
             return timeslider$('#innerdocbody').text().substring(0,10) == rev0text;
           }
           return false;
-        }).done(function(){
+        }, 5000).done(function(){
           timeslider$('#rightstep').click();
           helper.waitFor(function(){
             return timeslider$('#innerdocbody').text() == rev1text;
-          }).done(function(){
+          }, 3000).done(function(){
             timeslider$('#rightstep').click();
             helper.waitFor(function(){
               return timeslider$('#innerdocbody').text() == "\xa0"+rev2text;
-            }).done(function(){
+            }, 3000).done(function(){
               timeslider$('#leftstep').click();
               helper.waitFor(function(){
                 return timeslider$('#innerdocbody').text() == rev1text;
-              }).done(function(){
+              }, 3000).done(function(){
                 timeslider$('#leftstep').click();
                 helper.waitFor(function(){
                   return timeslider$('#innerdocbody').text().substring(0,10) == rev0text;
-                }).done(function(){
+                }, 3000).done(function(){
                   done();
                 })
               })
