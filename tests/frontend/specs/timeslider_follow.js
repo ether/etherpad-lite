8bedba23406adafb626cebb70d31c8fb7291d448
webzwo0i <webzwo0i@c3d2.de>
2020-10-18 20:13:48 +0200

    timeslider_follow: check if it's following to the correct lines

--- tests/frontend/specs/timeslider_follow.js
+++ tests/frontend/specs/timeslider_follow.js
@@ -5,7 +5,7 @@ describe("timeslider follow", function(){
   });
 
   it("content as it's added to timeslider", async function() {
-    // send 3 revisions
+    // send 6 revisions
     let revs = 6;
     let message = 'a\n\n\n\n\n\n\n\n\n\n';
     let newLines = message.split('\n').length
@@ -29,41 +29,90 @@ describe("timeslider follow", function(){
     })
   });
 
-  it("only to lines that exist in the current pad view #4389", async function(){
-    let rev0text = helper.textLines()[0];
-    let rev1text = "\xa0"; // timeslider will add &nbsp;
-    let rev2text = 'Test line'
-
-    helper.padInner$('body').empty();
+  /**
+   * Tests for bug described in #4389
+   * The goal is to scroll to the first line that contains a change right before
+   * the change is applied.
+   *
+   */
+  it("only to lines that exist in the current pad view, see #4389", async function(){
+    // Select everything and clear via delete key
+    let e = helper.padInner$.Event(helper.evtType);
+    e.keyCode = 8; //delete key
+    let lines = helper.linesDiv();
+    helper.selectLines(lines[0], lines[lines.length - 1]); // select all lines
+    // probably unnecessary, but wait for the selection to be Range not Caret
+    await helper.waitForPromise(function(){
+      return helper.padInner$.document.getSelection().type === 'Range';
+    })
+    helper.padInner$('#innerdocbody').trigger(e);
     await helper.waitForPromise(function(){
       return helper.commits.length === 1; 
     })
-    await helper.edit(rev2text)
+    await helper.edit("Test line\n\n")
+    await helper.edit("Another test line", 3)
 
     await helper.gotoTimeslider();
 
     // set to follow contents as it arrives
     helper.contentWindow().$('#options-followContents').prop("checked", true);
 
+    let oldYPosition = helper.contentWindow().$("#editorcontainerbox")[0].scrollTop;
+    expect(oldYPosition).to.be(0);
+
+    /**
+     * pad content rev 0 [default Pad text]
+     * pad content rev 1 ['']
+     * pad content rev 2 ['Test line','','']
+     * pad content rev 3 ['Test line','','Another test line']
+     */
+
+    // line 3 changed
     helper.contentWindow().$('#leftstep').click();
     await helper.waitForPromise(function(){
-      return helper.timesliderTextLines()[0] === rev1text;
+      return hasFollowedToLine(3);
     })
 
+    // line 1 is the first line that changed
     helper.contentWindow().$('#leftstep').click();
     await helper.waitForPromise(function(){
-      return helper.timesliderTextLines()[0] === rev0text;
+      return hasFollowedToLine(1);
     })
 
+    // line 1 changed
+    helper.contentWindow().$('#leftstep').click();
+    await helper.waitForPromise(function(){
+      return hasFollowedToLine(1);
+    })
+
+    // line 1 changed
     helper.contentWindow().$('#rightstep').click();
     await helper.waitForPromise(function(){
-      return helper.timesliderTextLines()[0] === rev1text;
+      return hasFollowedToLine(1);
     })
 
+    // line 1 is the first line that changed
+    helper.contentWindow().$('#rightstep').click();
+    await helper.waitForPromise(function(){
+      return hasFollowedToLine(1);
+    })
+
+    // line 3 changed
     helper.contentWindow().$('#rightstep').click();
     return helper.waitForPromise(function(){
-      return helper.timesliderTextLines()[0] === rev2text;
+      return hasFollowedToLine(3);
     })
   })
 });
+
+/**
+ * @param {number} lineNum 
+ * @returns {boolean} scrolled to the lineOffset?
+ */
+function hasFollowedToLine(lineNum) {
+  let scrollPosition = helper.contentWindow().$("#editorcontainerbox")[0].scrollTop;
+  let lineOffset = helper.contentWindow().$('#innerdocbody').find(`div:nth-child(${lineNum})`)[0].offsetTop;
+
+  return Math.abs(scrollPosition - lineOffset) < 1;
+}
 
