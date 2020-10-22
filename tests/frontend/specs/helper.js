a33b15288c1cf27fbeab77c927d86f6fb491935e
webzwo0i <webzwo0i@c3d2.de>
2020-10-22 15:55:36 +0200

    tests: relaxed constraint on the number of interval triggers

--- tests/frontend/specs/helper.js
+++ tests/frontend/specs/helper.js
@@ -151,8 +151,8 @@ describe("the test helper", function(){
       }, 2000, 100).fail(function(){
         // One at the beginning, and 19-20 more depending on whether it's the timeout or the final
         // poll that wins at 2000ms.
-        expect(checks).to.be.greaterThan(18);
-        expect(checks).to.be.lessThan(22);
+        expect(checks).to.be.greaterThan(17);
+        expect(checks).to.be.lessThan(24);
         done();
       });
     });
