63309632a619edc5654905eaa8e6c72433b9a43e
Richard Hansen <rhansen@rhansen.org>
2020-10-29 16:29:43 -0400

    updateCorePlugins.sh fixes
      * Add shebang
      * Exit on error
      * Use pathname expansion instead of `ls` command substitution
      * Avoid Bash-specific `[[` command
      * Quote variable expansion
      * Indent

--- bin/plugins/updateCorePlugins.sh
+++ bin/plugins/updateCorePlugins.sh
@@ -1,10 +1,9 @@
-for dir in `ls node_modules`;
-do
-# echo $0
-if [[ $dir == *"ep_"* ]]; then
-if [[ $dir != "ep_etherpad-lite" ]]; then
-node bin/plugins/checkPlugin.js $dir autofix autocommit
-fi
-fi
-# echo $dir
+#!/bin/sh
+
+set -e
+
+for dir in node_modules/ep_*; do
+  dir=${dir#node_modules/}
+  [ "$dir" != ep_etherpad-lite ] || continue
+  node bin/plugins/checkPlugin.js "$dir" autofix autocommit
 done
