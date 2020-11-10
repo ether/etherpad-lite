ether/etherpad-lite

commit 68a91f5e243ddfe0e5ae9d4080c64edbe2184289

HTML export: Move `padId` to context object property

src/node/utils/ExportHtml.js
@@ -498,10 +498,9 @@ exports.getPadHTMLDocument = async function (padId, revNum)
 
   let html = await getPadHTML(pad, revNum);
 
-  let exportHTMLAdditionalContent = await hooks.aCallAll("exportHTMLAdditionalContent", padId);
-  exportHTMLAdditionalContent.forEach(function(hookHtml){
+  for (const hookHtml of await hooks.aCallAll("exportHTMLAdditionalContent", {padId}) {
     html += hookHtml;
-  });
+  }
 
   return eejs.require("ep_etherpad-lite/templates/export_html.html", {
     body: html,

commit 8c55a38582e68c35bbebc63c8a22d86cd9b8bf67

HTML export: Add missing `)`
This bug was introduced in 68a91f5e243ddfe0e5ae9d4080c64edbe2184289.

src/node/utils/ExportHtml.js
@@ -498,7 +498,7 @@ exports.getPadHTMLDocument = async function (padId, revNum)
 
   let html = await getPadHTML(pad, revNum);
 
-  for (const hookHtml of await hooks.aCallAll("exportHTMLAdditionalContent", {padId}) {
+  for (const hookHtml of await hooks.aCallAll("exportHTMLAdditionalContent", {padId})) {
     html += hookHtml;
   }
 