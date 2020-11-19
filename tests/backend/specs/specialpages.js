ether/etherpad-lite

commit ed5a635f4c113ba7a535b51b30245adc5cfb6cf0

Add `req` to EJS render args when possible
This makes it possible for EJS templates and `eejsBlock_*` hook
functions to access the user's express-session state.

src/node/hooks/express/specialpages.js
@@ -14,13 +14,13 @@ exports.expressCreateServer = function (hook_name, args, cb) {
   //serve index.html under /
   args.app.get('/', function(req, res)
   {
-    res.send(eejs.require("ep_etherpad-lite/templates/index.html"));
+    res.send(eejs.require('ep_etherpad-lite/templates/index.html', {req}));
   });
 
   //serve javascript.html
   args.app.get('/javascript', function(req, res)
   {
-    res.send(eejs.require("ep_etherpad-lite/templates/javascript.html"));
+    res.send(eejs.require('ep_etherpad-lite/templates/javascript.html'), {req});
   });
 
 

src/node/hooks/express/admin.js
@@ -3,7 +3,7 @@ var eejs = require('ep_etherpad-lite/node/eejs');
 exports.expressCreateServer = function (hook_name, args, cb) {
   args.app.get('/admin', function(req, res) {
     if('/' != req.path[req.path.length-1]) return res.redirect('./admin/');
-    res.send( eejs.require("ep_etherpad-lite/templates/admin/index.html", {}) );
+    res.send(eejs.require('ep_etherpad-lite/templates/admin/index.html', {req}));
   });
   return cb();
 }

src/node/hooks/express/adminplugins.js
@@ -8,13 +8,12 @@ const UpdateCheck = require('ep_etherpad-lite/node/utils/UpdateCheck');
 
 exports.expressCreateServer = function(hook_name, args, cb) {
   args.app.get('/admin/plugins', function(req, res) {
-    var render_args = {
+    res.send(eejs.require('ep_etherpad-lite/templates/admin/plugins.html', {
       plugins: plugins.plugins,
+      req,
       search_results: {},
       errors: [],
-    };
-
-    res.send(eejs.require("ep_etherpad-lite/templates/admin/plugins.html", render_args));
+    }));
   });
 
   args.app.get('/admin/plugins/info', function(req, res) {
@@ -24,7 +23,8 @@ exports.expressCreateServer = function(hook_name, args, cb) {
     res.send(eejs.require("ep_etherpad-lite/templates/admin/plugins-info.html", {
       gitCommit: gitCommit,
       epVersion: epVersion,
-      latestVersion: UpdateCheck.getLatestVersion()
+      latestVersion: UpdateCheck.getLatestVersion(),
+      req,
     }));
   });
 

src/node/hooks/express/adminsettings.js
@@ -5,15 +5,12 @@ var fs = require('fs');
 
 exports.expressCreateServer = function (hook_name, args, cb) {
   args.app.get('/admin/settings', function(req, res) {
-
-    var render_args = {
+    res.send(eejs.require('ep_etherpad-lite/templates/admin/settings.html', {
+      req,
       settings: "",
       search_results: {},
       errors: []
-    };
-
-    res.send( eejs.require("ep_etherpad-lite/templates/admin/settings.html", render_args) );
-
+    }));
   });
   return cb();
 }

commit a05e8198c93ace093aa01ff23222890b831275a2

bugfix: Fix bad paren placement in `/javascript` handler (#4496)
* Fix bad paren placement in `/javascript` handler

This fixes a bug introduced in commit
ed5a635f4c113ba7a535b51b30245adc5cfb6cf0.

* add regression test for #4495

* Move `/javascript` test to `specialpages.js`

Co-authored-by: webzwo0i <webzwo0i@c3d2.de>

src/node/hooks/express/specialpages.js
@@ -20,7 +20,7 @@ exports.expressCreateServer = function (hook_name, args, cb) {
   //serve javascript.html
   args.app.get('/javascript', function(req, res)
   {
-    res.send(eejs.require('ep_etherpad-lite/templates/javascript.html'), {req});
+    res.send(eejs.require('ep_etherpad-lite/templates/javascript.html', {req}));
   });
 
 

tests/backend/specs/specialpages.js
@@ -1,0 +1,25 @@
+const common = require('../common');
+const settings = require('ep_etherpad-lite/node/utils/Settings');
+
+describe(__filename, function() {
+  let agent;
+  const backups = {};
+  before(async function() { agent = await common.init(); });
+  beforeEach(async function() {
+    backups.settings = {};
+    for (const setting of ['requireAuthentication', 'requireAuthorization']) {
+      backups.settings[setting] = settings[setting];
+    }
+    settings.requireAuthentication = false;
+    settings.requireAuthorization = false;
+  });
+  afterEach(async function() {
+    Object.assign(settings, backups.settings);
+  });
+
+  describe('/javascript', function() {
+    it('/javascript -> 200', async function() {
+      await agent.get('/javascript').expect(200);
+    });
+  });
+});