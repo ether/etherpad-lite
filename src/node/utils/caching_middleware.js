0dbec84794abdb90486109a0c6031d41a3136858
webzwo0i <webzwo0i@c3d2.de>
2020-11-21 01:48:43 +0100

    make the regex more clear

--- src/node/utils/caching_middleware.js
+++ src/node/utils/caching_middleware.js
@@ -196,7 +196,7 @@ CachingMiddleware.prototype = new function () {
         var statusCode = responseCache[cacheKey].statusCode;
 
         var pathStr = CACHE_DIR + 'minified_' + cacheKey;
-        if (supportsGzip && /^application\/javascript/.test(headers['content-type'])) {
+        if (supportsGzip && /application\/javascript/.test(headers['content-type'])) {
           pathStr = pathStr + '.gz';
           headers['content-encoding'] = 'gzip';
         }
