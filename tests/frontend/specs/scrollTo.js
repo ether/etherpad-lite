78fe277ac6aa75633edfa98fae5433b9ec41b329
John McLear <john@mclear.co.uk>
2020-12-11 14:30:16 +0000

    special king clothes

--- tests/frontend/specs/scrollTo.js
+++ tests/frontend/specs/scrollTo.js
@@ -10,7 +10,6 @@ describe('scrolls to line', function () {
     this.timeout(10000);
   });
 
-
   it('Scrolls down to Line 4', function (done) {
     this.timeout(10000);
     const chrome$ = helper.padChrome$;
@@ -18,6 +17,28 @@ describe('scrolls to line', function () {
       const topOffset = parseInt(chrome$('iframe').first('iframe')
           .contents().find('#outerdocbody').css('top'));
       return (topOffset >= 100);
+    });
+    done();
+  });
+});
+
+describe('doesnt break on weird hash input', function () {
+  // create a new pad with URL hash set before each test run
+  beforeEach(function (cb) {
+    helper.newPad({
+      hash: '#DEEZ123123NUTS',
+      cb,
+    });
+    this.timeout(10000);
+  });
+
+  it('Does NOT change scroll', function (done) {
+    this.timeout(10000);
+    const chrome$ = helper.padChrome$;
+    helper.waitFor(() => {
+      const topOffset = parseInt(chrome$('iframe').first('iframe')
+          .contents().find('#outerdocbody').css('top'));
+      return (!topOffset); // no css top should be set.
     });
     done();
   });
