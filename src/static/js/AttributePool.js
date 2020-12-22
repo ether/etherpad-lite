efc323cd71f5614269f8c681ffc4764f0a0447fb
John McLear <john@mclear.co.uk>
2020-12-22 14:49:11 +0000

    Revert "lint: attributepool.js"
    This reverts commit 33baaafbdeb4c0f82ec504738f5851f9ab5261a8.

--- src/static/js/AttributePool.js
+++ src/static/js/AttributePool.js
@@ -1,5 +1,3 @@
-'use strict';
-
 /**
  * This code represents the Attribute Pool Object of the original Etherpad.
  * 90% of the code is still like in the original Etherpad
@@ -71,7 +69,7 @@ AttributePool.prototype.getAttribValue = function (num) {
 };
 
 AttributePool.prototype.eachAttrib = function (func) {
-  for (const n of this.numToAttrib) {
+  for (const n in this.numToAttrib) {
     const pair = this.numToAttrib[n];
     func(pair[0], pair[1]);
   }
@@ -88,7 +86,7 @@ AttributePool.prototype.fromJsonable = function (obj) {
   this.numToAttrib = obj.numToAttrib;
   this.nextNum = obj.nextNum;
   this.attribToNum = {};
-  for (const n of this.numToAttrib) {
+  for (const n in this.numToAttrib) {
     this.attribToNum[String(this.numToAttrib[n])] = Number(n);
   }
   return this;
