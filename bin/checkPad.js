318c94f59972a3f0ad1ec1dc7f9626a786f63c77
Richard Hansen <rhansen@rhansen.org>
2021-01-17 03:16:01 -0500

    bin scripts: Delete redundant exception log messages
    The exception will cause Node.js to print the error message and stack
    trace so there's no point in logging it ourselves.

--- bin/checkPad.js
+++ bin/checkPad.js
@@ -19,74 +19,69 @@ let checkRevisionCount = 0;
 (async () => {
   await util.promisify(npm.load)({});
 
-  try {
-    // initialize database
-    require('ep_etherpad-lite/node/utils/Settings');
-    const db = require('ep_etherpad-lite/node/db/DB');
-    await db.init();
+  // initialize database
+  require('ep_etherpad-lite/node/utils/Settings');
+  const db = require('ep_etherpad-lite/node/db/DB');
+  await db.init();
 
-    // load modules
-    const Changeset = require('ep_etherpad-lite/static/js/Changeset');
-    const padManager = require('ep_etherpad-lite/node/db/PadManager');
+  // load modules
+  const Changeset = require('ep_etherpad-lite/static/js/Changeset');
+  const padManager = require('ep_etherpad-lite/node/db/PadManager');
 
-    const exists = await padManager.doesPadExists(padId);
-    if (!exists) throw new Error('Pad does not exist');
+  const exists = await padManager.doesPadExists(padId);
+  if (!exists) throw new Error('Pad does not exist');
 
-    // get the pad
-    const pad = await padManager.getPad(padId);
+  // get the pad
+  const pad = await padManager.getPad(padId);
 
-    // create an array with key revisions
-    // key revisions always save the full pad atext
-    const head = pad.getHeadRevisionNumber();
-    const keyRevisions = [];
-    for (let rev = 0; rev < head; rev += 100) {
-      keyRevisions.push(rev);
+  // create an array with key revisions
+  // key revisions always save the full pad atext
+  const head = pad.getHeadRevisionNumber();
+  const keyRevisions = [];
+  for (let rev = 0; rev < head; rev += 100) {
+    keyRevisions.push(rev);
+  }
+
+  // run through all key revisions
+  for (let keyRev of keyRevisions) {
+    keyRev = parseInt(keyRev);
+    // create an array of revisions we need till the next keyRevision or the End
+    const revisionsNeeded = [];
+    for (let rev = keyRev; rev <= keyRev + 100 && rev <= head; rev++) {
+      revisionsNeeded.push(rev);
     }
 
-    // run through all key revisions
-    for (let keyRev of keyRevisions) {
-      keyRev = parseInt(keyRev);
-      // create an array of revisions we need till the next keyRevision or the End
-      const revisionsNeeded = [];
-      for (let rev = keyRev; rev <= keyRev + 100 && rev <= head; rev++) {
-        revisionsNeeded.push(rev);
-      }
+    // this array will hold all revision changesets
+    const revisions = [];
 
-      // this array will hold all revision changesets
-      const revisions = [];
+    // run through all needed revisions and get them from the database
+    for (const revNum of revisionsNeeded) {
+      const revision = await db.get(`pad:${padId}:revs:${revNum}`);
+      revisions[revNum] = revision;
+    }
 
-      // run through all needed revisions and get them from the database
-      for (const revNum of revisionsNeeded) {
-        const revision = await db.get(`pad:${padId}:revs:${revNum}`);
-        revisions[revNum] = revision;
-      }
+    // check if the pad has a pool
+    if (pad.pool == null) throw new Error('Attribute pool is missing');
 
-      // check if the pad has a pool
-      if (pad.pool == null) throw new Error('Attribute pool is missing');
+    // check if there is an atext in the keyRevisions
+    let {meta: {atext} = {}} = revisions[keyRev] || {};
+    if (atext == null) {
+      console.error(`No atext in key revision ${keyRev}`);
+      continue;
+    }
 
-      // check if there is an atext in the keyRevisions
-      let {meta: {atext} = {}} = revisions[keyRev] || {};
-      if (atext == null) {
-        console.error(`No atext in key revision ${keyRev}`);
+    const apool = pad.pool;
+
+    for (let rev = keyRev + 1; rev <= keyRev + 100 && rev <= head; rev++) {
+      checkRevisionCount++;
+      try {
+        const cs = revisions[rev].changeset;
+        atext = Changeset.applyToAText(cs, atext, apool);
+      } catch (e) {
+        console.error(`Bad changeset at revision ${rev} - ${e.message}`);
         continue;
       }
-
-      const apool = pad.pool;
-
-      for (let rev = keyRev + 1; rev <= keyRev + 100 && rev <= head; rev++) {
-        checkRevisionCount++;
-        try {
-          const cs = revisions[rev].changeset;
-          atext = Changeset.applyToAText(cs, atext, apool);
-        } catch (e) {
-          console.error(`Bad changeset at revision ${rev} - ${e.message}`);
-          continue;
-        }
-      }
-      console.log(`Finished: Checked ${checkRevisionCount} revisions`);
     }
-  } catch (err) {
-    console.trace(err);
-    throw err;
+    console.log(`Finished: Checked ${checkRevisionCount} revisions`);
   }
 })();
