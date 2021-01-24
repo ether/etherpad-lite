c19fa54603f18c37acfb71eefde4abaec3c1fbe7
Richard Hansen <rhansen@rhansen.org>
2021-01-24 01:28:11 -0500

    tests: Re-enable import/export test that is now working

--- tests/backend/specs/api/importexport.js
+++ tests/backend/specs/api/importexport.js
@@ -31,7 +31,6 @@ const testImports = {
     input: '<html><body><ul> <li>FOO</li></ul></body></html>',
     expectedHTML: '<!DOCTYPE HTML><html><body><ul class="bullet"><li>FOO</ul><br></body></html>',
     expectedText: '\t* FOO\n\n',
-    disabled: true,
   },
   'prefixcorrectlinenumber': {
     input: '<html><body><ol><li>should be 1</li><li>should be 2</li></ol></body></html>',
