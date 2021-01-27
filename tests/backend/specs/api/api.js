e8e90ac207440cf03175cd5f82e6c999bf9d2633
John McLear <john@mclear.co.uk>
2021-01-27 21:51:22 +0000

    various timeout changes

--- tests/backend/specs/api/api.js
+++ tests/backend/specs/api/api.js
@@ -37,7 +37,7 @@ describe(__filename, function () {
 
   describe('OpenAPI definition', function () {
     it('generates valid openapi definition document', function (done) {
-      this.timeout(5000);
+      this.timeout(10000);
       api
           .get('/api/openapi.json')
           .expect((res) => {
