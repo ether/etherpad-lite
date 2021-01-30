f999452df59bddb33a9c61072b744366c24b6353
John McLear <john@mclear.co.uk>
2021-01-30 07:33:53 +0000

    Don't include gron in this PR

--- bin/getTestRunnerTimings.sh
+++
@@ -1,2 +0,0 @@
-# Requires gron
-for file in src/mochawesome-report/mochawesome*.json; do gron $file; done | grep -F 'results[' | grep -F 'tests[' | grep -E '(fullTitle|duration)' | sed -r 's/.*= //' | sed -r 's/[;"]//g' | paste -d '$' - - | awk -F'$' '{if ($1 > durations[$2]) durations[$2] = $1} END{for (i in durations) print durations[i], i}' | sed 's/^[^0-9].*/0 \0/'
