c639e743d7d50727911e168d8ef1fe1d6e3711bb
John McLear <john@mclear.co.uk>
2021-02-06 20:13:35 +0000

    tests: frontend tests for admin UI

---
+++ src/tests/frontend/specs/adminroot.js
@@ -0,0 +1,29 @@
+'use strict';
+
+describe('Admin page', function () {
+  before(async function () {
+    let success = false;
+    $.ajax({
+      url: `${location.protocol}//admin:changeme@${location.hostname}:${location.port}/admin/`,
+      type: 'GET',
+      success: () => success = true,
+    });
+    await helper.waitForPromise(() => success === true);
+  });
+
+  beforeEach(async function () {
+    helper.newAdmin('');
+    await helper.waitForPromise(
+        () => helper.admin$ && helper.admin$('.menu').find('li').length >= 3);
+  });
+
+  it('Shows Plugin Manager Link', async function () {
+    helper.admin$('a[data-l10n-id="admin_plugins"]').is(':visible');
+  });
+  it('Shows Troubleshooting Info Link', async function () {
+    helper.admin$('a[data-l10n-id="admin_plugins_info"]').is(':visible');
+  });
+  it('Shows Settings Link', async function () {
+    helper.admin$('a[data-l10n-id="admin_settings"]').is(':visible');
+  });
+});
