de40ba9cf2f955b9a31fb9de29930f04c208996c
John McLear <john@mclear.co.uk>
2021-02-12 16:43:41 +0000

    admin tests: longer timeouts

--- src/tests/frontend/specs/adminroot.js
+++ src/tests/frontend/specs/adminroot.js
@@ -14,19 +14,19 @@ describe('Admin page', function () {
   beforeEach(async function () {
     helper.newAdmin('');
     await helper.waitForPromise(
-        () => helper.admin$ && helper.admin$('.menu').find('li').length >= 3, 5000);
+        () => helper.admin$ && helper.admin$('.menu').find('li').length >= 3, 20000);
   });
 
   it('Shows Plugin Manager Link', async function () {
     helper.waitForPromise(
-        () => helper.admin$('a[data-l10n-id="admin_plugins"]').is(':visible'), 5000);
+        () => helper.admin$('a[data-l10n-id="admin_plugins"]').is(':visible'), 20000);
   });
   it('Shows Troubleshooting Info Link', async function () {
     helper.waitForPromise(
-        () => helper.admin$('a[data-l10n-id="admin_plugins_info"]').is(':visible'), 5000);
+        () => helper.admin$('a[data-l10n-id="admin_plugins_info"]').is(':visible'), 20000);
   });
   it('Shows Settings Link', async function () {
     helper.waitForPromise(
-        () => helper.admin$('a[data-l10n-id="admin_settings"]').is(':visible'), 5000);
+        () => helper.admin$('a[data-l10n-id="admin_settings"]').is(':visible'), 20000);
   });
 });
