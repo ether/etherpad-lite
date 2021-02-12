bd93f79a739c4edbfe04e7948b57d6b18785740b
John McLear <john@mclear.co.uk>
2021-02-12 13:46:57 +0000

    maxage language file while at it

--- src/node/hooks/i18n.js
+++ src/node/hooks/i18n.js
@@ -109,6 +109,7 @@ exports.expressCreateServer = (n, args, cb) => {
     // works with /locale/en and /locale/en.json requests
     const locale = req.params.locale.split('.')[0];
     if (exports.availableLangs.hasOwnProperty(locale)) {
+      res.setHeader('Cache-Control', `public, max-age=${settings.maxAge}`);
       res.setHeader('Content-Type', 'application/json; charset=utf-8');
       res.send(`{"${locale}":${JSON.stringify(locales[locale])}}`);
     } else {
