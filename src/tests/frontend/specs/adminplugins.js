c17c51ad876d09e4d2f21e11d8bf21f2f07e0c68
John McLear <john@mclear.co.uk>
2021-02-12 16:38:37 +0000

    admin tests: longer timeouts

--- src/tests/frontend/specs/adminplugins.js
+++ src/tests/frontend/specs/adminplugins.js
@@ -72,39 +72,39 @@ describe('Plugins page', function () {
   it('Attempt to Install a plugin', async function () {
     this.timeout(280000);
 
-    helper.admin$('#search-query').val('ep_activepads');
+    helper.admin$('#search-query').val('ep_headings2');
     await helper.waitForPromise(() => helper.admin$('.results').children().length < 300, 6000);
     await helper.waitForPromise(() => helper.admin$('.results').children().length > 0, 6000);
 
-    // skip if we already have ep_activepads installed..
-    if (helper.admin$('.ep_activepads .do-install').is(':visible') === false) this.skip();
+    // skip if we already have ep_headings2 installed..
+    if (helper.admin$('.ep_headings2 .do-install').is(':visible') === false) this.skip();
 
-    helper.admin$('.ep_activepads .do-install').click();
+    helper.admin$('.ep_headings2 .do-install').click();
     // ensure install has attempted to be started
     await helper.waitForPromise(
-        () => helper.admin$('.ep_activepads .do-install').length !== 0, 120000);
+        () => helper.admin$('.ep_headings2 .do-install').length !== 0, 120000);
     // ensure its not showing installing any more
     await helper.waitForPromise(
-        () => helper.admin$('.ep_activepads .message').text() === '', 180000);
+        () => helper.admin$('.ep_headings2 .message').text() === '', 180000);
     // ensure uninstall button is visible
     await helper.waitForPromise(
-        () => helper.admin$('.ep_activepads .do-uninstall').length !== 0, 120000);
+        () => helper.admin$('.ep_headings2 .do-uninstall').length !== 0, 120000);
   });
 
   it('Attempt to Uninstall a plugin', async function () {
     this.timeout(360000);
     await helper.waitForPromise(
-        () => helper.admin$('.ep_activepads .do-uninstall').length !== 0, 120000);
+        () => helper.admin$('.ep_headings2 .do-uninstall').length !== 0, 120000);
 
-    helper.admin$('.ep_activepads .do-uninstall').click();
+    helper.admin$('.ep_headings2 .do-uninstall').click();
 
     // ensure its showing uninstalling
     await helper.waitForPromise(
-        () => helper.admin$('.ep_activepads .message')
+        () => helper.admin$('.ep_headings2 .message')
             .text() === 'Uninstalling', 120000);
     // ensure its gone
     await helper.waitForPromise(
-        () => helper.admin$('.ep_activepads').length === 0, 240000);
+        () => helper.admin$('.ep_headings2').length === 0, 240000);
 
     helper.admin$('#search-query').val('ep_font');
     await helper.waitForPromise(() => helper.admin$('.results').children().length < 300, 240000);
