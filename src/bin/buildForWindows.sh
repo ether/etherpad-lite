ae5b3bb6957e97610481f18d551f77d588f70079
John McLear <john@mclear.co.uk>
2021-02-15 10:19:47 +0000

    release: Exclude `var/` during Windows build

--- src/bin/buildForWindows.sh
+++ src/bin/buildForWindows.sh
@@ -54,7 +54,7 @@ rm -rf "$TMP_FOLDER"/src/node_modules/nodemailer/node_modules/mailcomposer/node_
 
 log "create the zip..."
 cd "$TMP_FOLDER"
-zip -9 -r "$START_FOLDER"/etherpad-lite-win.zip ./*
+zip -9 -r "$START_FOLDER"/etherpad-lite-win.zip ./* -x var
 
 log "clean up..."
 rm -rf "$TMP_FOLDER"
