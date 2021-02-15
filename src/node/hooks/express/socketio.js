c1f6752146009f4b0dd7cf97266a1d05ff9d83bc
Richard Hansen <rhansen@rhansen.org>
2021-02-15 00:32:54 -0500

    for squash
    * Put the `maxHttpBufferSize` setting inside a new `socketIo` object
      to provide some context.
    * Refine the description of the setting.
    * Move the new setting up next to the related
      `socketTransportProtocols` setting.
    * Mention the setting in `CHANGELOG.md`.

--- src/node/hooks/express/socketio.js
+++ src/node/hooks/express/socketio.js
@@ -74,7 +74,7 @@ exports.expressCreateServer = (hookName, args, cb) => {
      *   https://github.com/socketio/socket.io/issues/2276#issuecomment-147184662 (not totally true, actually, see above)
      */
     cookie: false,
-    maxHttpBufferSize: settings.maxHttpBufferSize,
+    maxHttpBufferSize: settings.socketIo.maxHttpBufferSize,
   });
 
   io.on('connect', (socket) => {
