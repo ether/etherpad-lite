2f4d7cf5bcc0c1fb4d51c71d318ad298b691c303
John McLear <john@mclear.co.uk>
2021-02-17 14:55:54 +0000

    lint: use strict

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // @license magnet:?xt=urn:btih:8e4f440f4c65981c5bf93c76d35135ba5064d8b7&dn=apache-2.0.txt Apache-2.0
 /**
  * Copyright 2011 Peter Martischka, Primary Technology.
@@ -15,8 +17,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-/* global $, customStart */
 
 function randomPadName() {
   // the number of distinct chars (64) is chosen to ensure that the selection will be uniform when
