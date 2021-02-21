613c7d85456be7dae0088d7152a903881c1cb389
John McLear <john@mclear.co.uk>
2021-02-21 20:15:45 +0000

    lint: ExportTxt.js

--- src/node/utils/ExportTxt.js
+++ src/node/utils/ExportTxt.js
@@ -202,7 +202,7 @@ const getTXTFromAtext = (pad, atext, authorColors) => {
 
     if (line.listTypeName !== 'number') {
       // We're no longer in an OL so we can reset counting
-      for (const key in listNumbers) {
+      for (const key of Object.keys(listNumbers)) {
         delete listNumbers[key];
       }
     }
