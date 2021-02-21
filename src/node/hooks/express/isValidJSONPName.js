a8adba99541eae62853da5b0d15163294a3664cb
John McLear <john@mclear.co.uk>
2021-02-21 18:36:28 +0000

    api: Simplify regex used to check jsonp

--- src/node/hooks/express/isValidJSONPName.js
+++ src/node/hooks/express/isValidJSONPName.js
@@ -67,7 +67,7 @@ const RESERVED_WORDS = [
   'yield',
 ];
 
-const regex = /^[a-zA-Z_$][0-9a-zA-Z_$]*(?:\[(?:".+"|'.+'|\d+)\])*?$/;
+const regex = /^[a-zA-Z_$][0-9a-zA-Z_$]*$/;
 
 module.exports.check = (inputStr) => {
   let isValid = true;
