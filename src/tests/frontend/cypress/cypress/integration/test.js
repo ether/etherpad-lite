f328ac162ac5dd703bde1751b94127f384f7a0aa
John McLear <john@mclear.co.uk>
2021-02-25 23:51:32 +0000

    Update test.js

--- src/tests/frontend/cypress/cypress/integration/test.js
+++ src/tests/frontend/cypress/cypress/integration/test.js
@@ -1,4 +1,33 @@
-it('works', () => {
-  expect(42).to.equal(21 + 21)
-  cy.visit('https://example.cypress.io')
+'use strict';
+
+/* eslint-disable-next-line mocha/no-mocha-arrows, mocha/no-synchronous-tests */
+it('Pad content exists', () => {
+  cy.visit('http://127.0.0.1:9001/p/test');
+  getIframeBodyOuter().find('.line-number:first').should('have.text', '1');
+  getIframeBodyInner().find('.ace-line').should('be.visible');
 });
+
+const getIframeBodyOuter = () => {
+  // get the iframe > document > body
+  // and retry until the body element is not empty
+  return cy
+  .get('iframe[name="ace_outer"]')
+  .its('0.contentDocument.body').should('not.be.empty')
+  // wraps "body" DOM element to allow
+  // chaining more Cypress commands, like ".find(...)"
+  // https://on.cypress.io/wrap
+  .then(cy.wrap)
+}
+
+
+const getIframeBodyInner = () => {
+  // get the iframe > document > body
+  // and retry until the body element is not empty
+  return cy
+  .get('iframe[name="ace_inner"]')
+  .its('0.contentDocument.body').should('not.be.empty')
+  // wraps "body" DOM element to allow
+  // chaining more Cypress commands, like ".find(...)"
+  // https://on.cypress.io/wrap
+  .then(cy.wrap)
+}
