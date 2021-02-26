3823dad86a7eb177c86ed95fd2db68e64d21e2d9
John McLear <john@mclear.co.uk>
2021-02-26 08:18:14 +0000

    Update test.js

--- src/tests/frontend/cypress/cypress/integration/test.js
+++ src/tests/frontend/cypress/cypress/integration/test.js
@@ -1,16 +1,16 @@
 'use strict';
 
-Cypress.Commands.add('iframe', { prevSubject: 'element' }, $iframe => {
-  return new Cypress.Promise(resolve => {
-      $iframe.ready(function() {
-        resolve($iframe.contents().find('body'));
-      });
+Cypress.Commands.add('iframe', { prevSubject: 'element' }, function($iframe) {
+  return new Cypress.Promise( function (resolve) {
+    $iframe.ready(function() {
+      resolve($iframe.contents().find('body'));
+    });
   });
 });
 
 /* globals cy */
 /* eslint-disable-next-line mocha/no-mocha-arrows, mocha/no-synchronous-tests */
-it('Pad content exists', () => { /* eslint-disable-line mocha/no-global-tests */
+it('Pad content exists', function() { /* eslint-disable-line mocha/no-global-tests */
   cy.visit('http://127.0.0.1:9001/p/test', {timeout: 20000});
   const outer = cy.get('iframe[name="ace_outer"]', {timeout: 20000}).iframe();
   const inner = outer.find('iframe[name="ace_inner"]', {timeout: 20000}).iframe();
