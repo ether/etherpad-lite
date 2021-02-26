f3e961a423d6903697d68686a937b10ca6680149
John McLear <john@mclear.co.uk>
2021-02-26 08:45:01 +0000

    gitigignore and working test

--- src/tests/frontend/cypress/cypress/integration/test.js
+++ src/tests/frontend/cypress/cypress/integration/test.js
@@ -11,10 +11,15 @@ Cypress.Commands.add('iframe', { prevSubject: 'element' }, function($iframe) {
 /* globals cy */
 /* eslint-disable-next-line mocha/no-mocha-arrows, mocha/no-synchronous-tests */
 it('Pad content exists', function() { /* eslint-disable-line mocha/no-global-tests */
-  cy.visit('http://127.0.0.1:9001/p/test', {timeout: 20000});
-  const outer = cy.get('iframe[name="ace_outer"]', {timeout: 20000}).iframe();
-  const inner = outer.find('iframe[name="ace_inner"]', {timeout: 20000}).iframe();
-
-  outer.find('.line-number:first').should('have.text', '1');
-  inner.find('.ace-line:first').should('be.visible');
+  cy.visit('http://127.0.0.1:9001/p/test', {timeout: 60000});
+  cy.wait(2000)
+  cy.get('iframe[name="ace_outer"]', {timeout: 10000}).iframe()
+      .find('.line-number:first', {timeout: 10000})
+      .should('have.text', '1')
+  cy.wait(500)
+  cy.get('iframe[name="ace_outer"]', {timeout: 10000}).iframe()
+      .find('iframe[name="ace_inner"]', {timeout: 10000}).iframe()
+      .find('.ace-line:first')
+      .should('be.visible')
+      .should('have.text', 'Welcome to Etherpad!');
 });
