3785ab225c0d305a422076dc54ea2f4155bd4956
John McLear <john@mclear.co.uk>
2021-02-26 07:43:06 +0000

    timeouts

--- src/tests/frontend/cypress/cypress/integration/test.js
+++ src/tests/frontend/cypress/cypress/integration/test.js
@@ -2,9 +2,9 @@
 /* globals cy */
 /* eslint-disable-next-line mocha/no-mocha-arrows, mocha/no-synchronous-tests */
 it('Pad content exists', () => { /* eslint-disable-line mocha/no-global-tests */
-  cy.visit('http://127.0.0.1:9001/p/test');
-  const outer = cy.get('iframe[name="ace_outer"]').iframe();
-  const inner = outer.find('iframe[name="ace_inner"]').iframe();
+  cy.visit('http://127.0.0.1:9001/p/test', {timeout: 20000});
+  const outer = cy.get('iframe[name="ace_outer"]', {timeout: 20000}).iframe();
+  const inner = outer.find('iframe[name="ace_inner"]', {timeout: 20000}).iframe();
 
   outer.find('.line-number:first').should('have.text', '1');
   inner.find('.ace-line:first').should('be.visible');
