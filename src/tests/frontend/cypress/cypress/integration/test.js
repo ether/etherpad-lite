47b2126b13ace1335e6e8fc5c72b7c627a73dd0f
John McLear <john@mclear.co.uk>
2021-02-26 07:40:35 +0000

    rewrite

--- src/tests/frontend/cypress/cypress/integration/test.js
+++ src/tests/frontend/cypress/cypress/integration/test.js
@@ -3,17 +3,9 @@
 /* eslint-disable-next-line mocha/no-mocha-arrows, mocha/no-synchronous-tests */
 it('Pad content exists', () => { /* eslint-disable-line mocha/no-global-tests */
   cy.visit('http://127.0.0.1:9001/p/test');
-  getIframeBody('ace_outer').find('.line-number:first').should('have.text', '1');
-  cy.get('iframe[name="ace_outer"]').iframe().find('iframe[name="ace_inner"]').iframe()
-      .find('.ace-line:first').should('be.visible');
-});
+  const outer = cy.get('iframe[name="ace_outer"]').iframe();
+  const inner = outer.find('iframe[name="ace_inner"]').iframe();
 
-// get the iframe > document > body
-// and retry until the body element is not empty
-const getIframeBody = (iframeName) => cy
-    .get(`iframe[name="${iframeName}"]`)
-    .its('0.contentDocument.body').should('not.be.empty')
-    .then(cy.wrap);
-// wraps "body" DOM element to allow
-// chaining more Cypress commands, like ".find(...)"
-// https://on.cypress.io/wrap
+  outer.find('.line-number:first').should('have.text', '1');
+  inner.find('.ace-line:first').should('be.visible');
+});
