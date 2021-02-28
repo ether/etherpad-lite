8f80a7039abe417bb99f99b0be761568bcdeef9a
Richard Hansen <rhansen@rhansen.org>
2021-02-28 05:33:08 -0500

    for squash: lint fixes

--- src/tests/frontend/cypress/cypress/integration/test.js
+++ src/tests/frontend/cypress/cypress/integration/test.js
@@ -1,25 +1,27 @@
 'use strict';
 
-Cypress.Commands.add('iframe', { prevSubject: 'element' }, function($iframe) {
-  return new Cypress.Promise( function (resolve) {
-    $iframe.ready(function() {
-      resolve($iframe.contents().find('body'));
-    });
-  });
-});
+// TODO: See if there's an ESLint environment for Cypress.
+/* global Cypress, cy */
 
-/* globals cy */
-/* eslint-disable-next-line mocha/no-mocha-arrows, mocha/no-synchronous-tests */
-it('Pad content exists', function() { /* eslint-disable-line mocha/no-global-tests */
-  cy.visit('http://127.0.0.1:9001/p/test', {timeout: 60000});
-  cy.wait(2000)
-  cy.get('iframe[name="ace_outer"]', {timeout: 10000}).iframe()
-      .find('.line-number:first', {timeout: 10000})
-      .should('have.text', '1')
-  cy.wait(500)
-  cy.get('iframe[name="ace_outer"]', {timeout: 10000}).iframe()
-      .find('iframe[name="ace_inner"]', {timeout: 10000}).iframe()
-      .find('.ace-line:first')
-      .should('be.visible')
-      .should('have.text', 'Welcome to Etherpad!');
+Cypress.Commands.add('iframe', {prevSubject: 'element'},
+    ($iframe) => new Cypress.Promise((resolve) => {
+      $iframe.ready(() => {
+        resolve($iframe.contents().find('body'));
+      });
+    }));
+
+describe(__filename, function () {
+  it('Pad content exists', async function () {
+    cy.visit('http://127.0.0.1:9001/p/test', {timeout: 60000});
+    cy.wait(2000);
+    cy.get('iframe[name="ace_outer"]', {timeout: 10000}).iframe()
+        .find('.line-number:first', {timeout: 10000})
+        .should('have.text', '1');
+    cy.wait(500);
+    cy.get('iframe[name="ace_outer"]', {timeout: 10000}).iframe()
+        .find('iframe[name="ace_inner"]', {timeout: 10000}).iframe()
+        .find('.ace-line:first')
+        .should('be.visible')
+        .should('have.text', 'Welcome to Etherpad!');
+  });
 });
