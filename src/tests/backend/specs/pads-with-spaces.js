826c90ebd537064f9d579248eb1f0c7beedbd9ba
webzwo0i <webzwo0i@c3d2.de>
2021-02-28 01:25:04 +0100

    test with query

--- src/tests/backend/specs/pads-with-spaces.js
+++ src/tests/backend/specs/pads-with-spaces.js
@@ -15,4 +15,10 @@ describe(__filename, function () {
         .expect(302)
         .expect((res) => assert.equal(res.header.location, 'pads_with_spaces'));
   });
+
+  it('supports pads with spaces and query, regression test for #4883', async function () {
+    await agent.get('/p/pads with spaces?showChat=true&noColors=false')
+        .expect(302)
+        .expect((res) => assert.equal(res.header.location, 'pads_with_spaces?showChat=true&noColors=false'));
+  });
 });
