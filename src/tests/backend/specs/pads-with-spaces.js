c4c55a9ce9cdcd1340fc0570856ca132aafa4cdb
webzwo0i <webzwo0i@c3d2.de>
2021-02-28 01:46:10 +0100

    shorter syntax

--- src/tests/backend/specs/pads-with-spaces.js
+++ src/tests/backend/specs/pads-with-spaces.js
@@ -13,7 +13,7 @@ describe(__filename, function () {
   it('supports pads with spaces, regression test for #4883', async function () {
     await agent.get('/p/pads with spaces')
         .expect(302)
-        .expect((res) => assert.equal(res.header.location, 'pads_with_spaces'));
+        .expect('location', 'pads_with_spaces');
   });
 
   it('supports pads with spaces and query, regression test for #4883', async function () {
