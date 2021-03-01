64e9e7fcda79e8e354a783aeff66d69ad6d79faf
John McLear <john@mclear.co.uk>
2021-03-01 14:31:55 +0000

    tests:  Frontend test Windows ZIP (#4894)
    * tests:  Frontend test Windows ZIP
    
    This PR introduces Frontend testing within Github actions!
    
    We're depending a lot on saucelabs recently and that's fine but sometimes we just want to quickly do a frontend simple test on a weird environment (IE windows build) so this PR solves that problem.
    
    Things to note.
    
        It still builds the windows .zip if the cypress tests fail.
        It does not add any heavy deps to Etherpad as cypress must be installed in CI.
        Cypress is responsible for running the Etherpad instance.
    
    It's up to us how much we use this or not, I know it introduces a bunch of technical debt but I tried to keep that a minimum by compartmentalizing things and documenting where required.
    
    * Update .github/workflows/windows-zip.yml
    
    Co-authored-by: Richard Hansen <rhansen@rhansen.org>
    
    * remove timeouts
    
    * Move folder structure up a level
    
    * Update windows-zip.yml
    
    * Update test.js
    
    Co-authored-by: Richard Hansen <rhansen@rhansen.org>

---
+++ src/tests/frontend/cypress/integration/test.js
@@ -0,0 +1,22 @@
+'use strict';
+
+Cypress.Commands.add('iframe', {prevSubject: 'element'},
+    ($iframe) => new Cypress.Promise((resolve) => {
+      $iframe.ready(() => {
+        resolve($iframe.contents().find('body'));
+      });
+    }));
+
+describe(__filename, () => {
+  it('Pad content exists', async () => {
+    cy.visit('http://127.0.0.1:9001/p/test');
+    cy.get('iframe[name="ace_outer"]', {timeout: 10000}).iframe()
+        .find('.line-number:first')
+        .should('have.text', '1');
+    cy.get('iframe[name="ace_outer"]').iframe()
+        .find('iframe[name="ace_inner"]').iframe()
+        .find('.ace-line:first')
+        .should('be.visible')
+        .should('have.text', 'Welcome to Etherpad!');
+  });
+});
