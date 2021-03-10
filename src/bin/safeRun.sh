ether/etherpad-lite

commit 8b28e007842efefccf852a55f783761f86dc6090

restructure: Prefix `bin/` and `tests/` with `src/`
This is a follow-up to commit
2ea8ea12754c92ea06f22428f1e86341dcf00d99.

src/bin/safeRun.sh
@@ -23,8 +23,9 @@ fatal() { error "$@"; exit 1; }
 
 LAST_EMAIL_SEND=0
 
-# Move to the folder where ep-lite is installed
-cd "$(dirname "$0")"/..
+# Move to the Etherpad base directory.
+MY_DIR=$(try cd "${0%/*}" && try pwd -P) || exit 1
+try cd "${MY_DIR}/../.."
 
 # Check if a logfile parameter is set
 LOG="$1"
@@ -39,7 +40,7 @@ while true; do
   [ -w "${LOG}" ] || fatal "Logfile '${LOG}' is not writeable"
 
   # Start the application
-  bin/run.sh "$@" >>${LOG} 2>>${LOG}
+  src/bin/run.sh "$@" >>${LOG} 2>>${LOG}
 
   TIME_FMT=$(date +%Y-%m-%dT%H:%M:%S%z)
 

.github/workflows/backend-tests.yml
@@ -24,11 +24,11 @@ jobs:
         sudo apt install -y --no-install-recommends libreoffice libreoffice-pdfimport
 
     - name: Install all dependencies and symlink for ep_etherpad-lite
-      run: bin/installDeps.sh
+      run: src/bin/installDeps.sh
 
     # configures some settings and runs npm run test
     - name: Run the backend tests
-      run: tests/frontend/travis/runnerBackend.sh
+      run: src/tests/frontend/travis/runnerBackend.sh
 
   withplugins:
     # run on pushes to any branch
@@ -73,8 +73,8 @@ jobs:
     # if npm correctly hoists the dependencies, the hoisting seems to confuse
     # tools such as `npm outdated`, `npm update`, and some ESLint rules.
     - name: Install all dependencies and symlink for ep_etherpad-lite
-      run: bin/installDeps.sh
+      run: src/bin/installDeps.sh
 
     # configures some settings and runs npm run test
     - name: Run the backend tests
-      run: tests/frontend/travis/runnerBackend.sh
+      run: src/tests/frontend/travis/runnerBackend.sh

.github/workflows/dockerfile.yml
@@ -21,6 +21,6 @@ jobs:
       run: |
         docker build -t etherpad:test .
         docker run -d -p 9001:9001 etherpad:test
-        ./bin/installDeps.sh
+        ./src/bin/installDeps.sh
         sleep 3 # delay for startup?
         cd src && npm run test-container

.github/workflows/frontend-tests.yml
@@ -17,10 +17,10 @@ jobs:
         SAUCE_USERNAME: ${{ secrets.SAUCE_USERNAME }}
         SAUCE_ACCESS_KEY: ${{ secrets.SAUCE_ACCESS_KEY }}
         TRAVIS_JOB_NUMBER: ${{ github.run_id }}-${{ github.run_number }}-${{ github.job }}
-      run: tests/frontend/travis/sauce_tunnel.sh
+      run: src/tests/frontend/travis/sauce_tunnel.sh
 
     - name: Install all dependencies and symlink for ep_etherpad-lite
-      run: bin/installDeps.sh
+      run: src/bin/installDeps.sh
 
     - name: export GIT_HASH to env
       id: environment
@@ -37,7 +37,7 @@ jobs:
         TRAVIS_JOB_NUMBER: ${{ github.run_id }}-${{ github.run_number }}-${{ github.job }}
         GIT_HASH: ${{ steps.environment.outputs.sha_short }}
       run: |
-        tests/frontend/travis/runner.sh
+        src/tests/frontend/travis/runner.sh
 
   withplugins:
     name: with plugins
@@ -53,7 +53,7 @@ jobs:
         SAUCE_USERNAME: ${{ secrets.SAUCE_USERNAME }}
         SAUCE_ACCESS_KEY: ${{ secrets.SAUCE_ACCESS_KEY }}
         TRAVIS_JOB_NUMBER: ${{ github.run_id }}-${{ github.run_number }}-${{ github.job }}
-      run: tests/frontend/travis/sauce_tunnel.sh
+      run: src/tests/frontend/travis/sauce_tunnel.sh
 
     - name: Install Etherpad plugins
       run: >
@@ -79,7 +79,7 @@ jobs:
     # if npm correctly hoists the dependencies, the hoisting seems to confuse
     # tools such as `npm outdated`, `npm update`, and some ESLint rules.
     - name: Install all dependencies and symlink for ep_etherpad-lite
-      run: bin/installDeps.sh
+      run: src/bin/installDeps.sh
 
     - name: export GIT_HASH to env
       id: environment
@@ -90,7 +90,7 @@ jobs:
 
       # XXX we should probably run all tests, because plugins could effect their results
     - name: Remove standard frontend test files, so only plugin tests are run
-      run: rm tests/frontend/specs/*
+      run: rm src/tests/frontend/specs/*
 
     - name: Run the frontend tests
       shell: bash
@@ -100,4 +100,4 @@ jobs:
         TRAVIS_JOB_NUMBER: ${{ github.run_id }}-${{ github.run_number }}-${{ github.job }}
         GIT_HASH: ${{ steps.environment.outputs.sha_short }}
       run: |
-        tests/frontend/travis/runner.sh
+        src/tests/frontend/travis/runner.sh

.github/workflows/load-test.yml
@@ -18,13 +18,13 @@ jobs:
       uses: actions/checkout@v2
 
     - name: Install all dependencies and symlink for ep_etherpad-lite
-      run: bin/installDeps.sh
+      run: src/bin/installDeps.sh
 
     - name: Install etherpad-load-test
       run: sudo npm install -g etherpad-load-test
 
     - name: Run load test
-      run: tests/frontend/travis/runnerLoadTest.sh
+      run: src/tests/frontend/travis/runnerLoadTest.sh
 
   withplugins:
     # run on pushes to any branch
@@ -66,8 +66,8 @@ jobs:
     # if npm correctly hoists the dependencies, the hoisting seems to confuse
     # tools such as `npm outdated`, `npm update`, and some ESLint rules.
     - name: Install all dependencies and symlink for ep_etherpad-lite
-      run: bin/installDeps.sh
+      run: src/bin/installDeps.sh
 
     # configures some settings and runs npm run test
     - name: Run load test
-      run: tests/frontend/travis/runnerLoadTest.sh
+      run: src/tests/frontend/travis/runnerLoadTest.sh

.github/workflows/rate-limit.yml
@@ -22,8 +22,8 @@ jobs:
     - name: build docker image
       run: |
         docker build -f Dockerfile -t epl-debian-slim .
-        docker build -f tests/ratelimit/Dockerfile.nginx -t nginx-latest .
-        docker build -f tests/ratelimit/Dockerfile.anotherip -t anotherip .
+        docker build -f src/tests/ratelimit/Dockerfile.nginx -t nginx-latest .
+        docker build -f src/tests/ratelimit/Dockerfile.anotherip -t anotherip .
     - name: run docker images
       run: |
         docker run --name etherpad-docker -p 9000:9001 --rm --network ep_net --ip 172.23.42.2 -e 'TRUST_PROXY=true' epl-debian-slim &
@@ -31,9 +31,9 @@ jobs:
         docker run --rm --network ep_net --ip 172.23.42.3 --name anotherip -dt anotherip
 
     - name: install dependencies and create symlink for ep_etherpad-lite
-      run: bin/installDeps.sh
+      run: src/bin/installDeps.sh
 
     - name: run rate limit test
       run: |
-        cd tests/ratelimit
+        cd src/tests/ratelimit
         ./testlimits.sh

.gitignore
@@ -3,11 +3,8 @@ node_modules
 !settings.json.template
 APIKEY.txt
 SESSIONKEY.txt
-bin/abiword.exe
-bin/node.exe
 etherpad-lite-win.zip
 var/dirty.db
-bin/convertSettings.json
 *~
 *.patch
 npm-debug.log
@@ -15,9 +12,12 @@ npm-debug.log
 .ep_initialized
 *.crt
 *.key
-bin/etherpad-1.deb
 credentials.json
 out/
 .nyc_output
 ./package-lock.json
 .idea
+/src/bin/abiword.exe
+/src/bin/convertSettings.json
+/src/bin/etherpad-1.deb
+/src/bin/node.exe

.travis.yml
@@ -46,19 +46,19 @@ jobs:
       name: "Test the Frontend without Plugins"
       install:
         - *set_loglevel_warn
-        - "tests/frontend/travis/sauce_tunnel.sh"
-        - "bin/installDeps.sh"
+        - "src/tests/frontend/travis/sauce_tunnel.sh"
+        - "src/bin/installDeps.sh"
         - "export GIT_HASH=$(git rev-parse --verify --short HEAD)"
       script:
-        - "./tests/frontend/travis/runner.sh"
+        - "./src/tests/frontend/travis/runner.sh"
     - name: "Run the Backend tests without Plugins"
       install:
         - *install_libreoffice
         - *set_loglevel_warn
-        - "bin/installDeps.sh"
+        - "src/bin/installDeps.sh"
         - "cd src && npm install && cd -"
       script:
-        - "tests/frontend/travis/runnerBackend.sh"
+        - "src/tests/frontend/travis/runnerBackend.sh"
     - name: "Test the Dockerfile"
       install:
         - "cd src && npm install && cd -"
@@ -69,24 +69,24 @@ jobs:
     - name: "Load test Etherpad without Plugins"
       install:
         - *set_loglevel_warn
-        - "bin/installDeps.sh"
+        - "src/bin/installDeps.sh"
         - "cd src && npm install && cd -"
         - "npm install -g etherpad-load-test"
       script:
-        - "tests/frontend/travis/runnerLoadTest.sh"
+        - "src/tests/frontend/travis/runnerLoadTest.sh"
     # we can only frontend tests from the ether/ organization and not from forks.
     # To request tests to be run ask a maintainer to fork your repo to ether/
     - if: fork = false
       name: "Test the Frontend Plugins only"
       install:
         - *set_loglevel_warn
-        - "tests/frontend/travis/sauce_tunnel.sh"
-        - "bin/installDeps.sh"
-        - "rm tests/frontend/specs/*"
+        - "src/tests/frontend/travis/sauce_tunnel.sh"
+        - "src/bin/installDeps.sh"
+        - "rm src/tests/frontend/specs/*"
         - *install_plugins
         - "export GIT_HASH=$(git rev-parse --verify --short HEAD)"
       script:
-        - "./tests/frontend/travis/runner.sh"
+        - "./src/tests/frontend/travis/runner.sh"
     - name: "Lint test package-lock.json"
       install:
         - "npm install lockfile-lint"
@@ -96,11 +96,11 @@ jobs:
       install:
         - *install_libreoffice
         - *set_loglevel_warn
-        - "bin/installDeps.sh"
+        - "src/bin/installDeps.sh"
         - *install_plugins
         - "cd src && npm install && cd -"
       script:
-        - "tests/frontend/travis/runnerBackend.sh"
+        - "src/tests/frontend/travis/runnerBackend.sh"
     - name: "Test the Dockerfile"
       install:
         - "cd src && npm install && cd -"
@@ -111,24 +111,24 @@ jobs:
     - name: "Load test Etherpad with Plugins"
       install:
         - *set_loglevel_warn
-        - "bin/installDeps.sh"
+        - "src/bin/installDeps.sh"
         - *install_plugins
         - "cd src && npm install && cd -"
         - "npm install -g etherpad-load-test"
       script:
-        - "tests/frontend/travis/runnerLoadTest.sh"
+        - "src/tests/frontend/travis/runnerLoadTest.sh"
     - name: "Test rate limit"
       install:
         - "docker network create --subnet=172.23.42.0/16 ep_net"
         - "docker build -f Dockerfile -t epl-debian-slim ."
-        - "docker build -f tests/ratelimit/Dockerfile.nginx -t nginx-latest ."
-        - "docker build -f tests/ratelimit/Dockerfile.anotherip -t anotherip ."
+        - "docker build -f src/tests/ratelimit/Dockerfile.nginx -t nginx-latest ."
+        - "docker build -f src/tests/ratelimit/Dockerfile.anotherip -t anotherip ."
         - "docker run -p 8081:80 --rm --network ep_net --ip 172.23.42.1 -d nginx-latest"
         - "docker run --name etherpad-docker -p 9000:9001 --rm --network ep_net --ip 172.23.42.2 -e 'TRUST_PROXY=true' epl-debian-slim &"
         - "docker run --rm --network ep_net --ip 172.23.42.3 --name anotherip -dt anotherip"
-        - "./bin/installDeps.sh"
+        - "./src/bin/installDeps.sh"
       script:
-        - "cd tests/ratelimit && bash testlimits.sh"
+        - "cd src/tests/ratelimit && bash testlimits.sh"
 
 notifications:
   irc:

commit d2610284ad223be6fc930de4dbf414f3aac8aa59

bin/safeRun.sh: Fix `try: not found` bug
This fixes a copy+paste bug introduced in commit
8b28e007842efefccf852a55f783761f86dc6090 (v1.8.8).

src/bin/safeRun.sh
@@ -24,8 +24,8 @@ fatal() { error "$@"; exit 1; }
 LAST_EMAIL_SEND=0
 
 # Move to the Etherpad base directory.
-MY_DIR=$(try cd "${0%/*}" && try pwd -P) || exit 1
-try cd "${MY_DIR}/../.."
+MY_DIR=$(cd "${0%/*}" && pwd -P) || exit 1
+cd "${MY_DIR}/../.." || exit 1
 
 # Check if a logfile parameter is set
 LOG="$1"