ee41ca7807283288ca5407dd2300c5f13d5f4899
John McLear <john@mclear.co.uk>
2021-03-13 17:19:21 +0000

    Update collaborate.js

--- src/tests/frontend/cypress/integration/collaborate.js
+++ src/tests/frontend/cypress/integration/collaborate.js
@@ -10,14 +10,20 @@ Cypress.Commands.add('iframe', {prevSubject: 'element'},
 describe(__filename, () => {
   it('Pad content exists', () => {
     cy.visit('http://127.0.0.1:9001/p/collab');
-    cy.wait(10000); // wait for Minified JS to be built...
     cy.get('iframe[name="ace_outer"]', {timeout: 10000}).iframe()
         .find('.line-number:first')
         .should('have.text', '1');
     cy.get('iframe[name="ace_outer"]').iframe()
         .find('iframe[name="ace_inner"]').iframe()
         .find('.ace-line:first')
-        .should('be.visible')
-        .should('have.text', 'Welcome to Etherpad!');
+        .should('be.visible');
+    cy.get('iframe[name="ace_outer"]').iframe()
+        .find('iframe[name="ace_inner"]').iframe()
+        .find('.ace-line:first')
+        .type('Hello, World');
+    cy.get('iframe[name="ace_outer"]').iframe()
+        .find('iframe[name="ace_inner"]').iframe()
+        .find('.ace-line:first')
+        .type('Hello, World2');
   });
 });
