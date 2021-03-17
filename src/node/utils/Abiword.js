b6c2586920fdcc7d2c47c3e6629281322abd187e
Richard Hansen <rhansen@rhansen.org>
2021-03-17 18:17:07 -0400

    import/export: Delete unnecessary comments

--- src/node/utils/Abiword.js
+++ src/node/utils/Abiword.js
@@ -32,30 +32,16 @@ if (os.type().indexOf('Windows') > -1) {
   let stdoutBuffer = '';
 
   doConvertTask = (task, callback) => {
-    // span an abiword process to perform the conversion
     const abiword = spawn(settings.abiword, [`--to=${task.destFile}`, task.srcFile]);
-
-    // delegate the processing of stdout to another function
-    abiword.stdout.on('data', (data) => {
-      // add data to buffer
-      stdoutBuffer += data.toString();
-    });
-
-    // append error messages to the buffer
-    abiword.stderr.on('data', (data) => {
-      stdoutBuffer += data.toString();
-    });
-
-    // throw exceptions if abiword is dieing
+    abiword.stdout.on('data', (data) => { stdoutBuffer += data.toString(); });
+    abiword.stderr.on('data', (data) => { stdoutBuffer += data.toString(); });
     abiword.on('exit', (code) => {
       if (code !== 0) {
         return callback(`Abiword died with exit code ${code}`);
       }
-
       if (stdoutBuffer !== '') {
         console.log(stdoutBuffer);
       }
-
       callback();
     });
   };
@@ -67,45 +53,27 @@ if (os.type().indexOf('Windows') > -1) {
   // communicate with it via stdin/stdout
   // thats much faster, about factor 10
 } else {
-  // spawn the abiword process
   let abiword;
   let stdoutCallback = null;
   const spawnAbiword = () => {
     abiword = spawn(settings.abiword, ['--plugin', 'AbiCommand']);
     let stdoutBuffer = '';
     let firstPrompt = true;
-
-    // append error messages to the buffer
-    abiword.stderr.on('data', (data) => {
-      stdoutBuffer += data.toString();
-    });
-
-    // abiword died, let's restart abiword and return an error with the callback
+    abiword.stderr.on('data', (data) => { stdoutBuffer += data.toString(); });
     abiword.on('exit', (code) => {
       spawnAbiword();
       stdoutCallback(`Abiword died with exit code ${code}`);
     });
-
-    // delegate the processing of stdout to a other function
     abiword.stdout.on('data', (data) => {
-      // add data to buffer
       stdoutBuffer += data.toString();
-
       // we're searching for the prompt, cause this means everything we need is in the buffer
       if (stdoutBuffer.search('AbiWord:>') !== -1) {
-        // filter the feedback message
         const err = stdoutBuffer.search('OK') !== -1 ? null : stdoutBuffer;
-
-        // reset the buffer
         stdoutBuffer = '';
-
-        // call the callback with the error message
-        // skip the first prompt
         if (stdoutCallback != null && !firstPrompt) {
           stdoutCallback(err);
           stdoutCallback = null;
         }
-
         firstPrompt = false;
       }
     });
@@ -114,7 +82,6 @@ if (os.type().indexOf('Windows') > -1) {
 
   doConvertTask = (task, callback) => {
     abiword.stdin.write(`convert ${task.srcFile} ${task.destFile} ${task.type}\n`);
-    // create a callback that calls the task callback and the caller callback
     stdoutCallback = (err) => {
       callback();
       console.log('queue continue');
@@ -126,7 +93,6 @@ if (os.type().indexOf('Windows') > -1) {
     };
   };
 
-  // Queue with the converts we have to do
   const queue = async.queue(doConvertTask, 1);
   exports.convertFile = (srcFile, destFile, type, callback) => {
     queue.push({srcFile, destFile, type, callback});
