b164a34e64a8d3159fa2bb29f593804445e2d747
Richard Hansen <rhansen@rhansen.org>
2021-03-31 17:15:23 -0400

    lint: Fix ESLint error in `helper/methods.js`

--- src/tests/frontend/helper/methods.js
+++ src/tests/frontend/helper/methods.js
@@ -44,11 +44,7 @@ helper.edit = async (message, line) => {
  *
  * @returns {Array.<HTMLElement>} array of divs
  */
-helper.linesDiv = () => {
-  return helper.padInner$('.ace-line').map(function () {
-    return $(this);
-  }).get();
-};
+helper.linesDiv = () => helper.padInner$('.ace-line').map(function () { return $(this); }).get();
 
 /**
  * The pad text as an array of lines
