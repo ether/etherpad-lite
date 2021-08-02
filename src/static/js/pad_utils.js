0ca5a3459f808bad3edbc70bbf96344e695b3216
Richard Hansen <rhansen@rhansen.org>
2021-08-02 19:57:33 -0400

    Timeslider: Install an error handler

--- src/static/js/pad_utils.js
+++ src/static/js/pad_utils.js
@@ -296,6 +296,7 @@ const padutils = {
 let globalExceptionHandler = null;
 padutils.setupGlobalExceptionHandler = () => {
   if (globalExceptionHandler == null) {
+    require('./vendors/gritter');
     globalExceptionHandler = (e) => {
       let type;
       let err;
