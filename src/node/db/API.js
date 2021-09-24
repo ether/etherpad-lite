c87564f4b0e9947ceaf0bc3bc2c6d5c6d2708874
John McLear <john@mclear.co.uk>
2021-09-24 15:28:19 +0100

    breaking change / bugfix: ensure getText with rev only includes text and not nested text and attribs

--- src/node/db/API.js
+++ src/node/db/API.js
@@ -171,8 +171,12 @@ exports.getText = async (padID, rev) => {
     }
 
     // get the text of this revision
+    // getInternalRevisionAText method returns a text.text and attribs so we ensure we only
+    // return the text - details at https://github.com/ether/etherpad-lite/issues/5073
     const text = await pad.getInternalRevisionAText(rev);
-    return {text};
+    return {
+      text: text.text
+    };
   }
 
   // the client wants the latest text, lets return it to him
