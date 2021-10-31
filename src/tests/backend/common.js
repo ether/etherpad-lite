bea57ff249be2eb33b7881bed6e96a4d44c0d203
Richard Hansen <rhansen@rhansen.org>
2021-10-30 22:18:42 -0400

    tests: Use `logger` variable for consistency

--- src/tests/backend/common.js
+++ src/tests/backend/common.js
@@ -17,7 +17,8 @@ exports.baseUrl = null;
 exports.httpServer = null;
 exports.logger = log4js.getLogger('test');
 
-const logLevel = exports.logger.level;
+const logger = exports.logger;
+const logLevel = logger.level;
 
 // Mocha doesn't monitor unhandled Promise rejections, so convert them to uncaught exceptions.
 // https://github.com/mochajs/mocha/issues/2640
@@ -34,10 +35,10 @@ exports.init = async function () {
   agentPromise = new Promise((resolve) => { agentResolve = resolve; });
 
   if (!logLevel.isLessThanOrEqualTo(log4js.levels.DEBUG)) {
-    exports.logger.warn('Disabling non-test logging for the duration of the test. ' +
-                        'To enable non-test logging, change the loglevel setting to DEBUG.');
+    logger.warn('Disabling non-test logging for the duration of the test. ' +
+                'To enable non-test logging, change the loglevel setting to DEBUG.');
     log4js.setGlobalLogLevel(log4js.levels.OFF);
-    exports.logger.setLevel(logLevel);
+    logger.setLevel(logLevel);
   }
 
   // Note: This is only a shallow backup.
@@ -49,7 +50,7 @@ exports.init = async function () {
   settings.commitRateLimiting = {duration: 0.001, points: 1e6};
   exports.httpServer = await server.start();
   exports.baseUrl = `http://localhost:${exports.httpServer.address().port}`;
-  exports.logger.debug(`HTTP server at ${exports.baseUrl}`);
+  logger.debug(`HTTP server at ${exports.baseUrl}`);
   // Create a supertest user agent for the HTTP server.
   exports.agent = supertest(exports.baseUrl);
   // Speed up authn tests.
