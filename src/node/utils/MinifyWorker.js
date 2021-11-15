c94906f1f856e7bc7362253819c36f31eb464584
Richard Hansen <rhansen@rhansen.org>
2021-11-15 03:13:25 -0500

    MinifyWorker: Delete unhelpful comment
    The comment should have been a commit message instead.

--- src/node/utils/MinifyWorker.js
+++ src/node/utils/MinifyWorker.js
@@ -13,32 +13,6 @@ const compressJS = (content) => Terser.minify(content);
 const compressCSS = (filename, ROOT_DIR) => new Promise((res, rej) => {
   try {
     const absPath = path.resolve(ROOT_DIR, filename);
-
-    /*
-       * Changes done to migrate CleanCSS 3.x -> 4.x:
-       *
-       * 1. Rework the rebase logic, because the API was simplified (but we have
-       *    less control now). See:
-       *    https://github.com/jakubpawlowicz/clean-css/blob/08f3a74925524d30bbe7ac450979de0a8a9e54b2/README.md#important-40-breaking-changes
-       *
-       *    EXAMPLE:
-       *        The URLs contained in a CSS file (including all the stylesheets
-       *        imported by it) residing on disk at:
-       *            /home/muxator/etherpad/src/static/css/pad.css
-       *
-       *        Will be rewritten rebasing them to:
-       *            /home/muxator/etherpad/src/static/css
-       *
-       * 2. CleanCSS.minify() can either receive a string containing the CSS, or
-       *    an array of strings. In that case each array element is interpreted as
-       *    an absolute local path from which the CSS file is read.
-       *
-       *    In version 4.x, CleanCSS API was simplified, eliminating the
-       *    relativeTo parameter, and thus we cannot use our already loaded
-       *    "content" argument, but we have to wrap the absolute path to the CSS
-       *    in an array and ask the library to read it by itself.
-       */
-
     const basePath = path.dirname(absPath);
 
     new CleanCSS({
