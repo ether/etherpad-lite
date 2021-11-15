24b22e3a6df7781e5c147b7e189d78a230a696c0
Richard Hansen <rhansen@rhansen.org>
2021-11-15 03:14:04 -0500

    MinifyWorker: Fix buggy error handling in `compressCSS`

--- src/node/utils/MinifyWorker.js
+++ src/node/utils/MinifyWorker.js
@@ -5,14 +5,15 @@
 
 const CleanCSS = require('clean-css');
 const Terser = require('terser');
+const fsp = require('fs').promises;
 const path = require('path');
 const Threads = require('threads');
 
 const compressJS = (content) => Terser.minify(content);
 
 const compressCSS = async (filename, ROOT_DIR) => {
+  const absPath = path.resolve(ROOT_DIR, filename);
   try {
-    const absPath = path.resolve(ROOT_DIR, filename);
     const basePath = path.dirname(absPath);
     const output = await new CleanCSS({
       rebase: true,
@@ -22,7 +23,7 @@ const compressCSS = async (filename, ROOT_DIR) => {
   } catch (error) {
     // on error, just yield the un-minified original, but write a log message
     console.error(`Unexpected error minifying ${filename} (${absPath}): ${error}`);
-    callback(null, content);
+    return await fsp.readFile(absPath, 'utf8');
   }
 };
 
