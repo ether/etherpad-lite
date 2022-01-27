9db34244033335de8ac3ff8b1bf1a504eccc499d
Richard Hansen <rhansen@rhansen.org>
2022-01-27 00:23:02 -0500

    deps: Bump rehype and rehype-minify-whitespace

--- src/node/utils/ImportHtml.js
+++ src/node/utils/ImportHtml.js
@@ -19,13 +19,17 @@ const log4js = require('log4js');
 const Changeset = require('../../static/js/Changeset');
 const contentcollector = require('../../static/js/contentcollector');
 const jsdom = require('jsdom');
-const rehype = require('rehype');
-const minifyWhitespace = require('rehype-minify-whitespace');
 
 const apiLogger = log4js.getLogger('ImportHtml');
-const processor = rehype().use(minifyWhitespace, {newlines: false});
+let processor;
 
 exports.setPadHTML = async (pad, html) => {
+  if (processor == null) {
+    const [{rehype}, {default: minifyWhitespace}] =
+        await Promise.all([import('rehype'), import('rehype-minify-whitespace')]);
+    processor = rehype().use(minifyWhitespace, {newlines: false});
+  }
+
   html = String(await processor.process(html));
   const {window: {document}} = new jsdom.JSDOM(html);
 
