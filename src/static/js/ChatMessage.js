ether/etherpad-lite

commit 363a48b6d535c0a8501355a4d50f9885e2ee757e

ChatMessage: Log deprecation warnings for `.userId`, `.authorId`

src/static/js/ChatMessage.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const {padutils: {warnDeprecated}} = require('./pad_utils');
+
 /**
  * Represents a chat message stored in the database and transmitted among users. Plugins can extend
  * the object with additional properties.
@@ -52,8 +54,14 @@ class ChatMessage {
    * @deprecated Use `authorId` instead.
    * @type {string}
    */
-  get userId() { return this.authorId; }
-  set userId(val) { this.authorId = val; }
+  get userId() {
+    warnDeprecated('ChatMessage.userId property is deprecated; use .authorId instead');
+    return this.authorId;
+  }
+  set userId(val) {
+    warnDeprecated('ChatMessage.userId property is deprecated; use .authorId instead');
+    this.authorId = val;
+  }
 
   /**
    * Alias of `displayName`, for compatibility with old plugins.
@@ -61,8 +69,14 @@ class ChatMessage {
    * @deprecated Use `displayName` instead.
    * @type {string}
    */
-  get userName() { return this.displayName; }
-  set userName(val) { this.displayName = val; }
+  get userName() {
+    warnDeprecated('ChatMessage.userName property is deprecated; use .displayName instead');
+    return this.displayName;
+  }
+  set userName(val) {
+    warnDeprecated('ChatMessage.userName property is deprecated; use .displayName instead');
+    this.displayName = val;
+  }
 
   // TODO: Delete this method once users are unlikely to roll back to a version of Etherpad that
   // doesn't support authorId and displayName.

src/node/handler/PadMessageHandler.js
@@ -446,7 +446,7 @@ exports.sendChatMessageToPadClients = async (mt, puId, text = null, padId = null
   // pad.appendChatMessage() ignores the displayName property so we don't need to wait for
   // authorManager.getAuthorName() to resolve before saving the message to the database.
   const promise = pad.appendChatMessage(message);
-  message.displayName = await authorManager.getAuthorName(message.userId);
+  message.displayName = await authorManager.getAuthorName(message.authorId);
   socketio.sockets.in(padId).json.send({
     type: 'COLLABROOM',
     data: {type: 'CHAT_MESSAGE', message},

commit ad45359a9d67d8e818ac710af436d03632cf698e

chat: Silence accidental deprecation warnings
This fixes a bug introduced in commit
363a48b6d535c0a8501355a4d50f9885e2ee757e.

src/static/js/ChatMessage.js
@@ -10,6 +10,13 @@ const {padutils: {warnDeprecated}} = require('./pad_utils');
  */
 class ChatMessage {
   static fromObject(obj) {
+    // The userId property was renamed to authorId, and userName was renamed to displayName. Accept
+    // the old names in case the db record was written by an older version of Etherpad.
+    obj = Object.assign({}, obj); // Don't mutate the caller's object.
+    if ('userId' in obj && !('authorId' in obj)) obj.authorId = obj.userId;
+    delete obj.userId;
+    if ('userName' in obj && !('displayName' in obj)) obj.displayName = obj.userName;
+    delete obj.userName;
     return Object.assign(new ChatMessage(), obj);
   }
 