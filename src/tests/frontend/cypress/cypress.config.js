3fe42833a05575d10223ee96cf6c827b0430a461
webzwo0i <webzwo0i@c3d2.de>
2022-09-24 16:16:31 +0200

    fix cypress config

---
+++ src/tests/frontend/cypress/cypress.config.js
@@ -0,0 +1,9 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "http://127.0.0.1:9001",
+    supportFile: false,
+    specPattern: 'src/tests/frontend/cypress/integration/**/*.js'
+  }
+})
