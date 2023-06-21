54609af5e843301280934e2da11f0bbbd0ec118a
John McLear <john@mclear.co.uk>
2023-06-21 13:50:47 +0100

    github action fix for windows build

--- src/bin/buildForWindows.sh
+++ src/bin/buildForWindows.sh
@@ -19,6 +19,9 @@ workdir=$(try git rev-parse --show-toplevel) || exit 1
 try cd "${workdir}"
 [ -f src/package.json ] || fatal "failed to cd to etherpad root directory"
 
+# See https://github.com/msys2/MSYS2-packages/issues/1216
+export MSYS=winsymlinks:lnk
+
 OUTPUT=${workdir}/etherpad-win.zip
 
 TMP_FOLDER=$(try mktemp -d) || exit 1
