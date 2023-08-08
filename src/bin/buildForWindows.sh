a8281e48e0edbed007e58f10285e031b2f792980
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2023-08-08 16:04:01 +0200

    Use simple copy to copy the dependencies.

--- src/bin/buildForWindows.sh
+++ src/bin/buildForWindows.sh
@@ -33,7 +33,8 @@ try export GIT_WORK_TREE=${TMP_FOLDER}; git checkout HEAD -f \
     || fatal "failed to copy etherpad to temporary folder"
 try mkdir "${TMP_FOLDER}"/.git
 try git rev-parse HEAD >${TMP_FOLDER}/.git/HEAD
-try rsync -a ./src/node_modules "${TMP_FOLDER}"/src/
+try cp -r ./src/node_modules "${TMP_FOLDER}"/src/node_modules
+try cp -r node_modules "${TMP_FOLDER}"/node_modules
 
 try cd "${TMP_FOLDER}"
 [ -f src/package.json ] || fatal "failed to copy etherpad to temporary folder"
