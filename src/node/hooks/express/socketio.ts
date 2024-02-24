04cc3c8d54bc2387583e4196e934b58c8f3c40e0
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2024-02-24 13:40:25 +0100

    Fixed websocket connection.

--- src/node/hooks/express/socketio.ts
+++ src/node/hooks/express/socketio.ts
@@ -72,29 +72,7 @@ exports.expressCreateServer = (hookName:string, args:ArgsExpressType, cb:Functio
   // e.g. XHR is disabled in IE by default, so in IE it should use jsonp-polling
   io = new Server(args.server, {
     transports: settings.socketTransportProtocols,
-  }).listen(args.server, {
-    /*
-     * Do not set the "io" cookie.
-     *
-     * The "io" cookie is created by socket.io, and its purpose is to offer an
-     * handle to perform load balancing with session stickiness when the library
-     * falls back to long polling or below.
-     *
-     * In Etherpad's case, if an operator needs to load balance, he can use the
-     * "express_sid" cookie, and thus "io" is of no use.
-     *
-     * Moreover, socket.io API does not offer a way of setting the "secure" flag
-     * on it, and thus is a liability.
-     *
-     * Let's simply nuke "io".
-     *
-     * references:
-     *   https://socket.io/docs/using-multiple-nodes/#Sticky-load-balancing
-     *   https://github.com/socketio/socket.io/issues/2276#issuecomment-147184662 (not totally true, actually, see above)
-     */
-    cookie: false,
-    maxHttpBufferSize: settings.socketIo.maxHttpBufferSize,
-  });
+  })
 
   function handleConnection() {
     return (socket: any) => {
