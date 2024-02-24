55d41067b9cd1fe01b156dc78fdee8210985da87
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2024-02-24 09:48:06 +0100

    Added nextjs.

--- src/node/hooks/express/admin.ts
+++ src/node/hooks/express/admin.ts
@@ -2,7 +2,10 @@
 import {ArgsExpressType} from "../../types/ArgsExpressType";
 
 const eejs = require('../../eejs');
+import next from 'next'
 
+const app = next({dev: process.env.NODE_ENV !== 'production'})
+const handle = app.getRequestHandler()
 /**
  * Add the admin navigation link
  * @param hookName {String} the name of the hook
@@ -11,6 +14,15 @@ const eejs = require('../../eejs');
  * @return {*}
  */
 exports.expressCreateServer = (hookName:string, args: ArgsExpressType, cb:Function): any => {
+
+  args.app.get('/admin-new', (req:any, res:any) => {
+    return app.render(req, res, '/admin', req.query)
+  })
+
+  args.app.all('/admin/*', (req:any, res:any) => {
+    return handle(req, res)
+  })
+
   args.app.get('/admin', (req:any, res:any) => {
     if ('/' !== req.path[req.path.length - 1]) return res.redirect('./admin/');
     res.send(eejs.require('ep_etherpad-lite/templates/admin/index.html', {req}));
---
+++ src/pages/Admin.tsx
@@ -0,0 +1,7 @@
+export const Admin = ()=>{
+    return(
+        <div>
+            <h1>Admin</h1>
+        </div>
+    )
+}
