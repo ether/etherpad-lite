bf2b6b99a7b383e08d78ec21c681c9e081677a14
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2024-03-13 15:47:12 +0100

    Fixed tests

--- admin/src/pages/SettingsPage.tsx
+++ admin/src/pages/SettingsPage.tsx
@@ -3,7 +3,6 @@ import {isJSONClean} from "../utils/utils.ts";
 import {Trans} from "react-i18next";
 import {IconButton} from "../components/IconButton.tsx";
 import {RotateCw, Save} from "lucide-react";
-import Editor from '@monaco-editor/react';
 
 export const SettingsPage = ()=>{
     const settingsSocket = useStore(state=>state.settingsSocket)
@@ -11,18 +10,12 @@ export const SettingsPage = ()=>{
 
     return <div>
         <h1><Trans i18nKey="admin_settings.current"/></h1>
-        <Editor language="json" theme="vs-dark" options={{
-            comments: {
-                ignoreEmptyLines: true,
-                insertSpace: true
-            },
-            codeLens:false,
-            inDiffEditor: false
-        }} value={settings} onChange={(v)=>{
-            useStore.getState().setSettings(v!)
-        }}   className="settings"/>
+        <textarea value={settings} className="settings" onChange={v => {
+            useStore.getState().setSettings(v.target.value)
+        }}/>
         <div className="settings-button-bar">
-            <IconButton className="settingsButton" icon={<Save/>} title={<Trans i18nKey="admin_settings.current_save.value"/>} onClick={() => {
+            <IconButton className="settingsButton" icon={<Save/>}
+                        title={<Trans i18nKey="admin_settings.current_save.value"/>} onClick={() => {
                 if (isJSONClean(settings!)) {
                     // JSON is clean so emit it to the server
                     settingsSocket!.emit('saveSettings', settings!);
@@ -39,15 +32,18 @@ export const SettingsPage = ()=>{
                     })
                 }
             }}/>
-            <IconButton className="settingsButton" icon={<RotateCw/>} title={<Trans i18nKey="admin_settings.current_restart.value"/>}   onClick={() => {
+            <IconButton className="settingsButton" icon={<RotateCw/>}
+                        title={<Trans i18nKey="admin_settings.current_restart.value"/>} onClick={() => {
                 settingsSocket!.emit('restartServer');
             }}/>
         </div>
         <div className="separator"/>
         <div className="settings-button-bar">
-            <a rel="noopener noreferrer" target="_blank" href="https://github.com/ether/etherpad-lite/wiki/Example-Production-Settings.JSON"><Trans
+            <a rel="noopener noreferrer" target="_blank"
+               href="https://github.com/ether/etherpad-lite/wiki/Example-Production-Settings.JSON"><Trans
                 i18nKey="admin_settings.current_example-prod"/></a>
-            <a rel="noopener noreferrer" target="_blank" href="https://github.com/ether/etherpad-lite/wiki/Example-Development-Settings.JSON"><Trans
+            <a rel="noopener noreferrer" target="_blank"
+               href="https://github.com/ether/etherpad-lite/wiki/Example-Development-Settings.JSON"><Trans
                 i18nKey="admin_settings.current_example-devel"/></a>
         </div>
     </div>
