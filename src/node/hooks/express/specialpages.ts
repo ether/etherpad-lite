4ab21d90f74a484d9c75dd9f9ab0d2fc672b9e75
SamTv12345 <samtv12345@samtv12345.com>
2024-07-16 15:52:16 +0200

    Fixed loading

--- src/node/hooks/express/specialpages.ts
+++ src/node/hooks/express/specialpages.ts
@@ -1,13 +1,13 @@
 'use strict';
 
-const path = require('path');
+import path from 'node:path';
 const eejs = require('../../eejs')
-const fs = require('fs');
+import fs from 'node:fs';
 const fsp = fs.promises;
 const toolbar = require('../../utils/toolbar');
 const hooks = require('../../../static/js/pluginfw/hooks');
 const settings = require('../../utils/Settings');
-const util = require('util');
+import util from 'node:util';
 const webaccess = require('./webaccess');
 const plugins = require('../../../static/js/pluginfw/plugin_defs');
 import {hash, createHash} from 'node:crypto'
@@ -83,90 +83,109 @@ exports.expressCreateServer = async (hookName: string, args: any, cb: Function) 
     res.send(eejs.require('ep_etherpad-lite/templates/index.html', {req}));
   });
 
-  await fsp.writeFile(
-    path.join(settings.root, 'var/js/padbootstrap.js'),
-    eejs.require('ep_etherpad-lite/templates/padBootstrap.js', {
+
+  const padString =   eejs.require('ep_etherpad-lite/templates/padBootstrap.js', {
       pluginModules: (() => {
         const pluginModules = new Set();
         for (const part of plugins.parts) {
           for (const [, hookFnName] of Object.entries(part.client_hooks || {})) {
+            // @ts-ignore
             pluginModules.add(hookFnName.split(':')[0]);
           }
         }
         return [...pluginModules];
       })(),
       settings,
-    }));
+    })
 
-  await fsp.writeFile(
-    path.join(settings.root, 'var/js/timesliderBootstrap.js'),
-    eejs.require('ep_etherpad-lite/templates/timeSliderBootstrap.js', {
+
+    const timeSliderString = eejs.require('ep_etherpad-lite/templates/timeSliderBootstrap.js', {
       pluginModules: (() => {
         const pluginModules = new Set();
         for (const part of plugins.parts) {
           for (const [, hookFnName] of Object.entries(part.client_hooks || {})) {
+            // @ts-ignore
             pluginModules.add(hookFnName.split(':')[0]);
           }
         }
         return [...pluginModules];
       })(),
       settings,
-    }));
+    })
 
-  const hash = createHash('sha256').update(fs.readFileSync(path.join(settings.root, 'var/js/padbootstrap.js'))).digest('hex');
-  const hashTimeSlider = createHash('sha256').update(fs.readFileSync(path.join(settings.root, 'var/js/timesliderBootstrap.js'))).digest('hex');
 
-  const fileName = `padbootstrap-${hash.substring(0,16)}.min.js`
-  const fileNameTimeSlider = `timeSliderBootstrap-${hashTimeSlider.substring(0,16)}.min.js`
+
   const outdir = path.join(settings.root, 'var','js')
 
-
-
-  buildSync({
-    entryPoints: path.join(outdir, 'padbootstrap.js'), // Entry file(s)
+  const padWriteResult = buildSync({
+    stdin: {
+      contents: padString,
+      resolveDir: path.join(settings.root, 'var','js'),
+      loader: 'js'
+    }, // Entry file(s)
     bundle: true, // Bundle the files together
-    minify: false, // Minify the output
+    minify: process.env.NODE_ENV === "production", // Minify the output
+    sourcemap: true, // Generate source maps
+    sourceRoot: settings.root+"/src/static/js/",
+
+    target: ['es2020'], // Target ECMAScript version
+    metafile: true,
+
+    write: false, // Do not write to file system,
+  })
+
+  const outputPadJS  = padWriteResult.outputFiles[0].text
+
+   const timeSliderWrite = buildSync({
+    //entryPoints: [path.join(outdir, "timesliderBootstrap.js")], // Entry file(s),
+     stdin: {
+       contents: timeSliderString,
+       resolveDir: path.join(settings.root, 'var','js'),
+       loader: 'js'
+     },
+    bundle: true, // Bundle the files together
+    minify: process.env.NODE_ENV === "production", // Minify the output
     sourcemap: true, // Generate source maps
     sourceRoot: settings.root+"/src/static/js/",
     target: ['es2020'], // Target ECMAScript version
     metafile: true,
 
-    write: true, // Do not write to file system,
-    outfile: path.join(outdir,fileName), // Output file
+    write: false, // Do not write to file system,
   })
 
-   buildSync({
-    entryPoints: [settings.root + "/var/js/timesliderBootstrap.js"], // Entry file(s)
-    bundle: true, // Bundle the files together
-    minify: false, // Minify the output
-    sourcemap: true, // Generate source maps
-    sourceRoot: settings.root+"/src/static/js/",
-    target: ['es2020'], // Target ECMAScript version
-    metafile: true,
+  const outputTimeslider = timeSliderWrite.outputFiles[0].text
 
-    write: true, // Do not write to file system,
-    outfile: path.join(outdir,fileNameTimeSlider), // Output file
+  const hash = createHash('sha256').update(outputPadJS).digest('hex').substring(0,8);
+  const hashTimeSlider = createHash('sha256').update(outputTimeslider).digest('hex').substring(0,8);
+
+  const fileNamePad = `padbootstrap-${hash}.min.js`
+  const fileNameTimeSlider = `timeSliderBootstrap-${hashTimeSlider}.min.js`
+  const pathNamePad = path.join(outdir, fileNamePad)
+  const pathNameTimeSlider = path.join(outdir, fileNameTimeSlider)
+
+  if (!fs.existsSync(pathNamePad)) {
+    fs.writeFileSync(pathNamePad, outputPadJS);
+  }
+
+  if (!fs.existsSync(pathNameTimeSlider)) {
+    fs.writeFileSync(pathNameTimeSlider,outputTimeslider)
+  }
+
+  args.app.get("/"+fileNamePad, (req: any, res: any) => {
+    res.sendFile(pathNamePad)
   })
 
-
-  args.app.get(`/${fileName}`, (req: any, res: any) => {
-    res.sendFile(settings.root+`/var/js/${fileName}`)
+  args.app.get("/"+fileNamePad+".map", (req: any, res: any) => {
+    res.sendFile(pathNamePad+".map")
   })
 
-  args.app.get(`/${fileName}.map`, (req: any, res: any) => {
-    res.sendFile(settings.root+`/var/js/${fileName}.map`)
+  args.app.get("/"+fileNameTimeSlider, (req: any, res: any) => {
+    res.sendFile(pathNameTimeSlider)
   })
 
-  args.app.get(`/${fileNameTimeSlider}`, (req: any, res: any) => {
-    res.sendFile(settings.root+`/var/js/${fileNameTimeSlider}`)
+  args.app.get("/"+fileNameTimeSlider+".map", (req: any, res: any) => {
+    res.sendFile(pathNameTimeSlider+".map")
   })
-
-  args.app.get(`/${fileNameTimeSlider}.map`, (req: any, res: any) => {
-    res.sendFile(settings.root+`/var/js/${fileNameTimeSlider}.map`)
-  })
-
-
-
 
 
   // serve pad.html under /p
@@ -185,7 +204,7 @@ exports.expressCreateServer = async (hookName: string, args: any, cb: Function) 
       req,
       toolbar,
       isReadOnly,
-      entrypoint: "/"+fileName
+      entrypoint: "/"+fileNamePad
     }));
   });
 
