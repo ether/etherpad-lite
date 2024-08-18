32eae8cc2d9b65905dcbb5e6bdbc042becf8a05f
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2024-08-18 12:11:52 +0200

    Fixed test scopes

--- src/vitest.config.ts
+++ src/vitest.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    include: ["tests/backend-new/**/*.ts"],
+    include: ["tests/backend-new/specs/**/*.ts"],
   },
 })
