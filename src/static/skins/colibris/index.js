341691d44ac36e99884e61ea0c8679436e3151a7
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2025-07-29 17:17:21 +0200

    feat: started with recent pads implementation

--- src/static/skins/colibris/index.js
+++ src/static/skins/colibris/index.js
@@ -1,7 +1,41 @@
 'use strict';
 
+
 window.customStart = () => {
   // define your javascript here
   // jquery is available - except index.js
   // you can load extra scripts with $.getScript http://api.jquery.com/jQuery.getScript/
+  const recentPadList = document.getElementById('recent-pads');
+  const parentStyle = recentPadList.parentElement.style;
+  const recentPadListHeading = document.querySelector('[data-l10n-id="index.recentPads"]');
+  const recentPadsFromLocalStorage = localStorage.getItem('recent-pads');
+  let recentPadListData = [];
+  if (recentPadsFromLocalStorage != null) {
+    recentPadListData = JSON.parse(recentPadsFromLocalStorage);
+  }
+
+  if (recentPadListData.length === 0) {
+    recentPadListHeading.setAttribute('data-l10n-id', 'index.recentPadsEmpty');
+    parentStyle.display = 'flex';
+    parentStyle.justifyContent = 'center';
+    parentStyle.alignItems = 'center';
+    parentStyle.height = '100%';
+    recentPadList.remove();
+  } else {
+    /**
+     * @typedef {Object} Pad
+     * @property {string} name
+     */
+
+    /**
+     * @param {Pad} pad
+     */
+    recentPadListData.forEach((pad) => {
+      const li = document.createElement('li');
+      li.className = 'recent-pad';
+      const padPath = `${window.location.href}p/${pad.name}`;
+      li.innerHTML = `<a href="${padPath}">${pad}</a>`;
+      recentPadList.appendChild(li);
+    });
+  }
 };
