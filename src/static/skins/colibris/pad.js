b184b73e74e31587260dc76494f132758f3cbb06
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2025-07-29 20:55:58 +0200

    feat: first version

--- src/static/skins/colibris/pad.js
+++ src/static/skins/colibris/pad.js
@@ -6,7 +6,17 @@ window.customStart = () => {
   $('.buttonicon').on('mouseup', function () { $(this).parent().removeClass('pressed'); });
 
   const pathSegments = window.location.pathname.split('/').filter(Boolean);
-  const lastSegment = pathSegments[pathSegments.length - 1];
-  const padName = window.location.href
-
+  const padName = pathSegments[pathSegments.length - 1];
+  const recentPads = localStorage.getItem('recentPads');
+  if (recentPads == null) {
+    localStorage.setItem('recentPads', JSON.stringify([]));
+  }
+  const recentPadsList = JSON.parse(localStorage.getItem('recentPads'));
+  if (!recentPadsList.includes(padName)) {
+    if (recentPadsList.length >= 10) {
+      recentPadsList.shift(); // Remove the oldest pad if we have more than 10
+    }
+    recentPadsList.push(padName);
+    localStorage.setItem('recentPads', JSON.stringify(recentPadsList));
+  }
 };
