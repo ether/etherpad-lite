b8e61b4606433ab10ab0f2f49906189c7fb8fa68
SamTV12345 <40429738+SamTV12345@users.noreply.github.com>
2025-08-24 22:17:18 +0200

    chore: fixed git determination and fixed no skin css issues (#7089)

--- src/static/skins/no-skin/index.js
+++ src/static/skins/no-skin/index.js
@@ -4,4 +4,14 @@ window.customStart = () => {
   // define your javascript here
   // jquery is available - except index.js
   // you can load extra scripts with $.getScript http://api.jquery.com/jQuery.getScript/
+  const divHoldingPlaceHolderLabel = document
+      .querySelector('[data-l10n-id="index.placeholderPadEnter"]');
+
+  const observer = new MutationObserver(() => {
+    document.querySelector('#go2Name input')
+        .setAttribute('placeholder', divHoldingPlaceHolderLabel.textContent);
+  });
+
+  observer
+      .observe(divHoldingPlaceHolderLabel, {childList: true, subtree: true, characterData: true});
 };
