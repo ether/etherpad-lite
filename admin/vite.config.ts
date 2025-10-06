66f05bac0a8c24a2fef2d75922c53dcce080cffb
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2025-10-06 12:33:12 +0200

    chore: added react compiler and fixed url to manage pads

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import {viteStaticCopy} from "vite-plugin-static-copy";
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +11,10 @@ export default defineConfig({
               dest: ''
           }
       ]
-  })],
+  }),   react({
+    babel: {
+      plugins: ['babel-plugin-react-compiler'],
+    }})],
     base: '/admin',
     build:{
       outDir: '../src/templates/admin',
