b73ff8c4a1726686f29f69d185c446c2454ecbef
Phillip Wirth <phillip.wirth@dataport.de>
2025-10-16 10:25:47 +0200

    fix prometheus metric registration

--- src/node/prometheus.ts
+++ src/node/prometheus.ts
@@ -1,25 +1,21 @@
-import client from 'prom-client'
-const db = require('./db/DB').db
+import client from 'prom-client';
+const db = require('./db/DB').db;
+
+const register = new client.Registry();
+const gaugeDB = new client.Gauge({
+  name: 'ueberdb_stats',
+  help: 'ueberdb stats',
+  labelNames: ['type'],
+});
+register.registerMetric(gaugeDB);
+client.collectDefaultMetrics({register});
 
 const monitor = function () {
-  const collectDefaultMetrics = client.collectDefaultMetrics;
-  const Registry = client.Registry;
-  const register = new Registry();
-  collectDefaultMetrics({register});
-  const gaugeDB = new client.Gauge({
-    name: "ueberdb_stats",
-    help: "ueberdb stats",
-    labelNames: ['type'],
-  })
-
   for (const [metric, value] of Object.entries(db.metrics)) {
     if (typeof value !== 'number') continue;
     gaugeDB.set({type: metric}, value);
   }
-
-
-  register.registerMetric(gaugeDB);
-  return register
+  return register;
 };
 
 export default monitor;
